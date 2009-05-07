2d12bda068321907422da68ccf0957af75c38dc3
Marko Lahma <marko.lahma@gmail.com>
2009-05-07 04:28:06 +0000

    QRTZNET-148: Add SQL Server Compact Edition support for AdoJobStore

--- src/Quartz.Tests.Integration/Impl/AdoJobStore/AdoJobStoreSmokeTest.cs
+++ src/Quartz.Tests.Integration/Impl/AdoJobStore/AdoJobStoreSmokeTest.cs
@@ -25,6 +25,7 @@ namespace Quartz.Tests.Integration.Impl.AdoJobStore
             dbConnectionStrings["Oracle"] =
                 "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=xe)));User Id=quartznet;Password=quartznet;";
             dbConnectionStrings["SQLServer"] = "Server=(local);Database=quartz;Trusted_Connection=True;";
+            dbConnectionStrings["SQLServerCe"] = @"Data Source=C:\quartznet.sdf;Persist Security Info=False;";
             dbConnectionStrings["MySQL"] = "Server = localhost; Database = quartz; Uid = quartznet; Pwd = quartznet";
             dbConnectionStrings["PostgreSQL"] =
                 "Server=127.0.0.1;Port=5432;Userid=quartznet;Password=quartznet;Protocol=3;SSL=false; Pooling=true;MinPoolSize=1;MaxPoolSize=20;Encoding=UTF8;Timeout=15;SslMode=Disable;";
@@ -49,55 +50,92 @@ namespace Quartz.Tests.Integration.Impl.AdoJobStore
         [Test]
         public void TestSqlServer11()
         {
-            RunAdoJobStoreTest("SqlServer-11", "SQLServer");
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.SqlServerDelegate, Quartz";
+            RunAdoJobStoreTest("SqlServer-11", "SQLServer", properties);
         }
 
         [Test]
         public void TestSqlServer20()
         {
-            RunAdoJobStoreTest("SqlServer-20", "SQLServer");
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.SqlServerDelegate, Quartz";
+            RunAdoJobStoreTest("SqlServer-20", "SQLServer", properties);
         }
+
+
+        [Test]
+        public void TestSqlServerCe351()
+        {
+            bool previousClustered = clustered;
+            clustered = false;
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.SqlServerDelegate, Quartz";
+            try
+            {
+                RunAdoJobStoreTest("SqlServerCe-351", "SQLServerCe", properties);
+            }
+            finally
+            {
+                clustered = previousClustered;
+            }
+        }
+
 
         [Test]
         public void TestOracleClient20()
         {
-            RunAdoJobStoreTest("OracleClient-20", "Oracle");
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.OracleDelegate, Quartz";
+            RunAdoJobStoreTest("OracleClient-20", "Oracle", properties);
         }
 
         [Test]
         public void TestOracleODP20()
         {
-            RunAdoJobStoreTest("OracleODP-20", "Oracle");
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.OracleDelegate, Quartz";
+            RunAdoJobStoreTest("OracleODP-20", "Oracle", properties);
         }
 
         [Test]
         public void TestMySql50()
         {
-            RunAdoJobStoreTest("MySql-50", "MySQL");
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.MySQLDelegate, Quartz";
+            RunAdoJobStoreTest("MySql-50", "MySQL", properties);
         }
 
         [Test]
         public void TestMySql51()
         {
-            RunAdoJobStoreTest("MySql-51", "MySQL");
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.MySQLDelegate, Quartz";
+            RunAdoJobStoreTest("MySql-51", "MySQL", properties);
         }
 
         [Test]
         public void TestMySql10()
         {
-            RunAdoJobStoreTest("MySql-10", "MySQL");
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.MySQLDelegate, Quartz";
+            RunAdoJobStoreTest("MySql-10", "MySQL", properties);
         }
 
         [Test]
         public void TestMySql109()
         {
-            RunAdoJobStoreTest("MySql-109", "MySQL");
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.MySQLDelegate, Quartz";
+            RunAdoJobStoreTest("MySql-109", "MySQL", properties);
         }
 
         [Test]
         public void TestSQLite10()
         {
-            RunAdoJobStoreTest("SQLite-10", "SQLite");
+            NameValueCollection properties = new NameValueCollection();
+            properties["quartz.jobStore.driverDelegateType"] = "Quartz.Impl.AdoJobStore.SQLiteDelegate, Quartz";
+            RunAdoJobStoreTest("SQLite-10", "SQLite", properties);
         }
 
         [Test]
@@ -153,7 +191,12 @@ namespace Quartz.Tests.Integration.Impl.AdoJobStore
                     "Quartz.Impl.AdoJobStore.UpdateLockRowSemaphore, Quartz";
             }
 
-            properties["quartz.dataSource.default.connectionString"] = (string) dbConnectionStrings[connectionStringId];
+            string connectionString;
+            if (!dbConnectionStrings.TryGetValue(connectionStringId, out connectionString))
+            {
+                throw new Exception("Unknown connection string id: " + connectionStringId);
+            }
+            properties["quartz.dataSource.default.connectionString"] = connectionString;
             properties["quartz.dataSource.default.provider"] = dbProvider;
 
             // First we must get a reference to a scheduler
