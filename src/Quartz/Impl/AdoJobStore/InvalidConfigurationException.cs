ced2dbd7720b536c4a5327329f139dddfcc9c8dc
Marko Lahma <marko.lahma@gmail.com>
2009-12-04 13:14:41 +0000

    Update authorship from OpenSymphony to James House

--- src/Quartz/Impl/AdoJobStore/InvalidConfigurationException.cs
+++ src/Quartz/Impl/AdoJobStore/InvalidConfigurationException.cs
@@ -1,5 +1,5 @@
 /* 
-* Copyright 2004-2005 OpenSymphony 
+* Copyright 2004-2009 James House 
 * 
 * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
 * use this file except in compliance with the License. You may obtain a copy 
