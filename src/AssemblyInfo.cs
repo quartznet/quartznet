48a3ef285c44c77cab264c0ecf79b9a45d18a1c7
Marko Lahma <marko.lahma@gmail.com>
2009-12-12 15:00:24 +0000

    Fixed missing using directive

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
