fe782c80b3989fe18052f83cc16c2f61f94745c8
Marko Lahma <marko.lahma@gmail.com>
2011-07-08 22:21:18 +0300

    Fixed documentation

--- src/Quartz/MisfireInstruction.cs
+++ src/Quartz/MisfireInstruction.cs
@@ -44,8 +44,12 @@ namespace Quartz
         /// and then update the Trigger as if it had fired at the proper time.
         /// </summary>
         /// <remarks>
-        /// NOTE: if a trigger uses this instruction, and it has missed
-        /// several of its scheduled firings, then
+        /// NOTE: if a trigger uses this instruction, and it has missed 
+        /// several of its scheduled firings, then several rapid firings may occur 
+        /// as the trigger attempt to catch back up to where it would have been. 
+        /// For example, a SimpleTrigger that fires every 15 seconds which has 
+        /// misfired for 5 minutes will fire 20 times once it gets the chance to 
+        /// fire.
         /// </remarks>
         public const int IgnoreMisfirePolicy = -1;
 
