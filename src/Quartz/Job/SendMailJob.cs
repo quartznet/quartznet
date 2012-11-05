c2677d5cc150fe71e4819151ad8b3d526e11732d
ed <edwardt.tril@gmail.com>
2012-11-05 12:08:43 -0800

    Added back using pattern around smtp client on SendMailJob

--- src/Quartz/Job/SendMailJob.cs
+++ src/Quartz/Job/SendMailJob.cs
@@ -26,110 +26,110 @@ using Common.Logging;
 
 namespace Quartz.Job
 {
-	/// <summary>
-	/// A Job which sends an e-mail with the configured content to the configured
-	/// recipient.
-	/// </summary>
-	/// <author>James House</author>
-	/// <author>Marko Lahma (.NET)</author>
-	public class SendMailJob : IJob
-	{
-		private static readonly ILog Log = LogManager.GetLogger(typeof (SendMailJob));
+    /// <summary>
+    /// A Job which sends an e-mail with the configured content to the configured
+    /// recipient.
+    /// </summary>
+    /// <author>James House</author>
+    /// <author>Marko Lahma (.NET)</author>
+    public class SendMailJob : IJob
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(SendMailJob));
 
-    	/// <summary> The host name of the smtp server. REQUIRED.</summary>
-		public const string PropertySmtpHost = "smtp_host";
+        /// <summary> The host name of the smtp server. REQUIRED.</summary>
+        public const string PropertySmtpHost = "smtp_host";
 
-		/// <summary> The e-mail address to send the mail to. REQUIRED.</summary>
-		public const string PropertyRecipient = "recipient";
+        /// <summary> The e-mail address to send the mail to. REQUIRED.</summary>
+        public const string PropertyRecipient = "recipient";
 
-		/// <summary> The e-mail address to cc the mail to. Optional.</summary>
-		public const string PropertyCcRecipient = "cc_recipient";
+        /// <summary> The e-mail address to cc the mail to. Optional.</summary>
+        public const string PropertyCcRecipient = "cc_recipient";
 
-		/// <summary> The e-mail address to claim the mail is from. REQUIRED.</summary>
-		public const string PropertySender = "sender";
+        /// <summary> The e-mail address to claim the mail is from. REQUIRED.</summary>
+        public const string PropertySender = "sender";
 
-		/// <summary> The e-mail address the message should say to reply to. Optional.</summary>
-		public const string PropertyReplyTo = "reply_to";
+        /// <summary> The e-mail address the message should say to reply to. Optional.</summary>
+        public const string PropertyReplyTo = "reply_to";
 
-		/// <summary> The subject to place on the e-mail. REQUIRED.</summary>
-		public const string PropertySubject = "subject";
+        /// <summary> The subject to place on the e-mail. REQUIRED.</summary>
+        public const string PropertySubject = "subject";
 
-		/// <summary> The e-mail message body. REQUIRED.</summary>
-		public const string PropertyMessage = "message";
+        /// <summary> The e-mail message body. REQUIRED.</summary>
+        public const string PropertyMessage = "message";
 
-		/// <summary>
-		/// Executes the job.
-		/// </summary>
-		/// <param name="context">The job execution context.</param>
+        /// <summary>
+        /// Executes the job.
+        /// </summary>
+        /// <param name="context">The job execution context.</param>
         public virtual void Execute(IJobExecutionContext context)
-		{
-			JobDataMap data = context.JobDetail.JobDataMap;
+        {
+            JobDataMap data = context.JobDetail.JobDataMap;
 
-			string smtpHost = data.GetString(PropertySmtpHost);
-			string to = data.GetString(PropertyRecipient);
-			string cc = data.GetString(PropertyCcRecipient);
-			string from = data.GetString(PropertySender);
-			string replyTo = data.GetString(PropertyReplyTo);
-			string subject = data.GetString(PropertySubject);
-			string message = data.GetString(PropertyMessage);
+            string smtpHost = data.GetString(PropertySmtpHost);
+            string to = data.GetString(PropertyRecipient);
+            string cc = data.GetString(PropertyCcRecipient);
+            string from = data.GetString(PropertySender);
+            string replyTo = data.GetString(PropertyReplyTo);
+            string subject = data.GetString(PropertySubject);
+            string message = data.GetString(PropertyMessage);
 
-			if (smtpHost == null || smtpHost.Trim().Length == 0)
-			{
-				throw new ArgumentException("PropertySmtpHost not specified.");
-			}
-			if (to == null || to.Trim().Length == 0)
-			{
-				throw new ArgumentException("PropertyRecipient not specified.");
-			}
-			if (from == null || from.Trim().Length == 0)
-			{
-				throw new ArgumentException("PropertySender not specified.");
-			}
-			if (subject == null || subject.Trim().Length == 0)
-			{
-				throw new ArgumentException("PropertySubject not specified.");
-			}
-			if (message == null || message.Trim().Length == 0)
-			{
-				throw new ArgumentException("PropertyMessage not specified.");
-			}
+            if (smtpHost == null || smtpHost.Trim().Length == 0)
+            {
+                throw new ArgumentException("PropertySmtpHost not specified.");
+            }
+            if (to == null || to.Trim().Length == 0)
+            {
+                throw new ArgumentException("PropertyRecipient not specified.");
+            }
+            if (from == null || from.Trim().Length == 0)
+            {
+                throw new ArgumentException("PropertySender not specified.");
+            }
+            if (subject == null || subject.Trim().Length == 0)
+            {
+                throw new ArgumentException("PropertySubject not specified.");
+            }
+            if (message == null || message.Trim().Length == 0)
+            {
+                throw new ArgumentException("PropertyMessage not specified.");
+            }
 
-			if (cc != null && cc.Trim().Length == 0)
-			{
-				cc = null;
-			}
+            if (cc != null && cc.Trim().Length == 0)
+            {
+                cc = null;
+            }
 
-			if (replyTo != null && replyTo.Trim().Length == 0)
-			{
-				replyTo = null;
-			}
+            if (replyTo != null && replyTo.Trim().Length == 0)
+            {
+                replyTo = null;
+            }
 
-			string mailDesc = string.Format(CultureInfo.InvariantCulture, "'{0}' to: {1}", subject, to);
+            string mailDesc = string.Format(CultureInfo.InvariantCulture, "'{0}' to: {1}", subject, to);
 
-			Log.Info(string.Format(CultureInfo.InvariantCulture, "Sending message {0}", mailDesc));
+            Log.Info(string.Format(CultureInfo.InvariantCulture, "Sending message {0}", mailDesc));
 
-			try
-			{
-				SendMail(smtpHost, to, cc, from, replyTo, subject, message);
-			}
-			catch (Exception ex)
-			{
-				throw new JobExecutionException(string.Format(CultureInfo.InvariantCulture, "Unable to send mail: {0}", mailDesc), ex, false);
-			}
-		}
+            try
+            {
+                SendMail(smtpHost, to, cc, from, replyTo, subject, message);
+            }
+            catch (Exception ex)
+            {
+                throw new JobExecutionException(string.Format(CultureInfo.InvariantCulture, "Unable to send mail: {0}", mailDesc), ex, false);
+            }
+        }
 
 
-	    private void SendMail(string smtpHost, string to, string cc, string from, string replyTo, string subject,
-		                      string message)
-		{
+        private void SendMail(string smtpHost, string to, string cc, string from, string replyTo, string subject,
+                              string message)
+        {
 
             MailMessage mimeMessage = new MailMessage();
-			mimeMessage.To.Add(to);
-            if (!String.IsNullOrEmpty(cc)) 
+            mimeMessage.To.Add(to);
+            if (!String.IsNullOrEmpty(cc))
             {
                 mimeMessage.CC.Add(cc);
             }
-			mimeMessage.From = new MailAddress(from);
+            mimeMessage.From = new MailAddress(from);
             if (!String.IsNullOrEmpty(replyTo))
             {
 #if NET_40
@@ -138,16 +138,19 @@ namespace Quartz.Job
                 mimeMessage.ReplyTo = new MailAddress(replyTo);
 #endif
             }
-			mimeMessage.Subject = subject;
-			mimeMessage.Body = message;
+            mimeMessage.Subject = subject;
+            mimeMessage.Body = message;
 
             Send(mimeMessage, smtpHost);
-		}
+        }
 
-	    protected virtual void Send(MailMessage mimeMessage, string smtpHost)
-	    {
-            SmtpClient client = new SmtpClient(smtpHost);
-	        client.Send(mimeMessage);
-	    }
-	}
+        protected virtual void Send(MailMessage mimeMessage, string smtpHost)
+        {
+            using (SmtpClient client = new SmtpClient(smtpHost))
+            {
+                client.Send(mimeMessage);
+            }
+
+        }
+    }
 }
