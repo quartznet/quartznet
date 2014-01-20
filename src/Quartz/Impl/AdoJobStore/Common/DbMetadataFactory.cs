bdbcbe9f66e50d3aa1976ffb1679cfb8f6d3527e
DrakOOnIC <drakoonic@hotmail.com>
2014-01-20 22:22:19 +0100

    The DbProvider updated, so now we can define the Database metadata in the quartz configuration section of the application too (which has a precedence over the embedded configuration)
    Implementation details:
     - DbMetadataFactory: abstraction for metadata creation/query.
     - EmbeddedAssemblyResourceDbMetadataFactory:  the existing behavior
     - ConfigurationBasedDbMetadataFactory: new configuration based behavior

---
+++ src/Quartz/Impl/AdoJobStore/Common/DbMetadataFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quartz.Impl.AdoJobStore.Common
+{
+    /// <summary>
+    /// Base class for the DbMetadata Factory implentations
+    /// </summary>
+    public abstract class DbMetadataFactory
+    {
+        /// <summary>
+        /// Gets the supported provider names.
+        /// </summary>
+        /// <returns>The enumeration of the supported provider names</returns>
+        public abstract IEnumerable<string> GetProviderNames();
+
+        /// <summary>
+        /// Gets the database metadata assotiated to the specified provider name.
+        /// </summary>
+        /// <param name="providerName">Name of the provider.</param>
+        /// <returns>The metadata instance for the requested provider</returns>
+        public abstract DbMetadata GetDbMetadata(string providerName);
+    }
+}
