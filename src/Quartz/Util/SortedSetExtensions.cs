6ed5342a65688d0e3d16a1588301c97aaf610738
Marko Lahma <marko.lahma@gmail.com>
2014-08-03 18:49:55 +0300

    use SortedSet instead of TreeSet / remove C5 lib

---
+++ src/Quartz/Util/SortedSetExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Quartz.Util
+{
+    internal static class SortedSetExtensions
+    {
+        internal static SortedSet<int> TailSet(this SortedSet<int> set, int value)
+        {
+            return set.GetViewBetween(value, 9999999);
+        }
+    }
+};
