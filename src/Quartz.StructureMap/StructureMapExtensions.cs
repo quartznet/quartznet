833597a63bcafac5a601478924efcf1f50b8ca77
DixonD-git <dixond@acm.lviv.ua>
2015-08-24 19:41:11 +0300

    Implemented integration with StructureMap.

---
+++ src/Quartz.StructureMap/StructureMapExtensions.cs
@@ -0,0 +1,13 @@
+﻿using StructureMap;
+
+namespace Quartz.StructureMap
+{
+    public static class StructureMapExtensions
+    {
+        public static IScheduler UseStructureMap(this IScheduler scheduler, IContainer container)
+        {
+            scheduler.JobFactory = new StructureMapJobFactory(container);
+            return scheduler;
+        }
+    }
+}
