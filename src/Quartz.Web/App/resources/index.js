743ff535331e08e3e3e1f01fc0c49e1f758fcf4b
Marko Lahma <marko.lahma@gmail.com>
2015-09-20 14:32:15 +0300

    lib upgrades

--- src/Quartz.Web/App/resources/index.js
+++ src/Quartz.Web/App/resources/index.js
@@ -1,6 +1,6 @@
 define(["require", "exports"], function (require, exports) {
     function configure(aurelia) {
-        aurelia.globalizeResources("./date-format", "./duration-format");
+        aurelia.globalResources("./date-format", "./duration-format");
     }
     exports.configure = configure;
 });
