15f63a5db67bd2ed556006b8ef3afc9f78e96c5a
Chris Knight <cknightdevelopment@gmail.com>
2015-11-19 11:21:52 -0800

    Moved DirectoryScanJobModel to the Quartz.Job namespace.

--- src/Quartz/Job/DirectoryScanJobModel.cs
+++ src/Quartz/Job/DirectoryScanJobModel.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Quartz.Job;
 using Quartz.Simpl;
 using Quartz.Spi;
 
-namespace Quartz
+namespace Quartz.Job
 {
     /// <summary>
     /// Internal model to hold settings used by <see cref="DirectoryScanJob"/>
