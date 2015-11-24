5780746d39bdbf33ddf65e999e495c8d77169741
Kent Chenery <kent.chenery@xero.com>
2015-11-25 12:22:58 +1300

    Set length of binary type to -1

--- src/Quartz/Impl/AdoJobStore/SqlServerDelegate.cs
+++ src/Quartz/Impl/AdoJobStore/SqlServerDelegate.cs
@@ -50,7 +50,7 @@ namespace Quartz.Impl.AdoJobStore
             {
                 paramValue = (bool) paramValue ? 1 : 0;
             }
-
+            
             base.AddCommandParameter(cmd, paramName, paramValue, dataType);
         }
     }
--- src/Quartz/Impl/AdoJobStore/StdAdoDelegate.cs
+++ src/Quartz/Impl/AdoJobStore/StdAdoDelegate.cs
@@ -2894,7 +2894,20 @@ namespace Quartz.Impl.AdoJobStore
 
         public virtual void AddCommandParameter(IDbCommand cmd, string paramName, object paramValue, Enum dataType)
         {
-            adoUtil.AddCommandParameter(cmd, paramName, paramValue, dataType);
+            if ((dataType != null) && (dataType.Equals(dbProvider.Metadata.DbBinaryType)))
+            {
+                adoUtil.AddCommandParameter(cmd, paramName, paramValue, dataType, -1);
+            }
+            else
+            {
+                adoUtil.AddCommandParameter(cmd, paramName, paramValue, dataType);
+            }
+        }
+
+        public virtual void AddCommandParameter(IDbCommand cmd, string paramName, object paramValue, Enum dataType, int size)
+        {
+            
+            adoUtil.AddCommandParameter(cmd, paramName, paramValue, dataType, size);
         }
     }
 }
