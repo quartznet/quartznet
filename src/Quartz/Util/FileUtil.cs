ab4cfaf2b74c5e9ac54186d98ac0e3913a858989
Mike Rousos <MikeRou@Microsoft.com>
2016-04-14 12:07:35 -0700

    Add AppContext support

--- src/Quartz/Util/FileUtil.cs
+++ src/Quartz/Util/FileUtil.cs
@@ -66,11 +66,11 @@ namespace Quartz.Util
                 }
                 try
                 {
-#if APPDOMAINS
-                    fName = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, fName);
-#else // APPDOMAINS
+#if APPCONTEXT
                     fName = Path.Combine(AppContext.BaseDirectory, fName);
-#endif // APPDOMAINS
+#else // APPCONTEXT
+                    fName = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, fName);
+#endif // APPCONTEXT
                 }
                 catch (SecurityException)
                 {
