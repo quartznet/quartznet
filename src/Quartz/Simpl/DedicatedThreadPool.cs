d61ba4acbbff7ec9ef8ad9cc620f8e9156270e63
Mike Rousos <MikeRou@Microsoft.com>
2016-05-02 11:21:15 -0700

    Clean-up and comment the new IThreadPool implementations

--- src/Quartz/Simpl/DedicatedThreadPool.cs
+++ src/Quartz/Simpl/DedicatedThreadPool.cs
@@ -3,8 +3,16 @@ using Quartz.Util;
 
 namespace Quartz.Simpl
 {
+    /// <summary>
+    /// An implementation of the TaskSchedulingThreadPool which uses a custom task scheduler
+    /// with a dedicated pool of threads reserved only for its own scheduling purposes
+    /// </summary>
     public class DedicatedThreadPool : TaskSchedulingThreadPool
     {
+        /// <summary>
+        /// Returns a QueuedTaskScheduler
+        /// </summary>
+        /// <returns>QueuedTaskScheduler with threadCount == MaxConcurrency</returns>
         protected override TaskScheduler GetDefaultScheduler()
         {
             return new QueuedTaskScheduler(MaxConcurency);
