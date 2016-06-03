585c35f2191250a4c68c6a67ddde69a1fda16681
Kristjan Laane <kristjan.laane@gmail.com>
2016-06-03 12:47:19 +0300

    Manual test for ScheduledFireTimeUtc property of JobExecutionContext The test fails with the following output: (Test started at 09:35 UTC) (Computer put to sleep at 09:36 UTC) (Trigger should have fired at 09:37 UTC but could not because the computer was in sleep) (Computer woken up at 09:41 UTC) Event Scheduled for 03/06/2016 09:41:37 +00:00 Ticked at 03/06/2016 09:41:37 +00:00 with Difference of 00:00:00.0260000 Scheduled Fire Time Wrong - It Should Have Been Earlier Because of the Misfire   Expected: greater than 00:00:30   But was:  00:00:00.0260000
    A fixed job was needed to implement this test, so a fixed job factory was also added, which can be used in any circumstance when only one job is executed for all triggers.
    In order to ask the tester to carry out the manual part of this test at the right time a MessageBox is used, which is why the build script is modified a bit as well to include a Windows Forms reference.

---
+++ src/Quartz.Tests.Integration/ScheduledFireTimeTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Common.Logging;
+
+using NUnit.Framework;
+
+using Quartz.Impl;
+using Quartz.Simpl;
+
+namespace Quartz.Tests.Integration
+{
+    /// <summary>
+    /// This is a manual test requiring the tester to manually change the system clock at the right time or put the device to sleep for the right time 
+    /// Not inheriting from <see cref="IntegrationTest" /> so that this manual test would not be run when integration category tests are run
+    /// </summary>
+    [TestFixture, Explicit]
+    public class ScheduledFireTimeTest
+    {
+        private IScheduler sched;
+        private ScheduledFireTimeTestJob scheduledFireTimeTestJob;
+
+        [SetUp]
+        public void SetUp()
+        {
+            ISchedulerFactory sf = new StdSchedulerFactory();
+            sched = sf.GetScheduler();            
+            scheduledFireTimeTestJob = new ScheduledFireTimeTestJob();
+            var jobFactory = new FixedJobFactory(scheduledFireTimeTestJob);            
+            sched.JobFactory = jobFactory;            
+        }
+
+        [Test]
+        public void TestScheduledFireTimeUtc()
+        {
+            var now = DateTimeOffset.Now;
+            var startTime = now.AddSeconds(120);
+            var trigger = TriggerBuilder.Create().WithIdentity("DailyTrigger")
+                .WithSchedule(
+                    DailyTimeIntervalScheduleBuilder.Create()
+                        .WithMisfireHandlingInstructionFireAndProceed()
+                        .StartingDailyAt(TimeOfDay.HourMinuteAndSecondOfDay(startTime.Hour, startTime.Minute, startTime.Second))
+                        .OnEveryDay()
+                        .WithIntervalInHours(1)
+                        .WithRepeatCount(9999)
+                        .InTimeZone(TimeZoneInfo.Local)
+                ).Build();
+            var jobDetail = JobBuilder.Create().WithIdentity("DailyJob", "DailyGroup").Build();
+            sched.ScheduleJob(jobDetail, trigger);
+            sched.Start();            
+            MessageBox.Show("You have about a minute to cause a misfire now. Please either: " + Environment.NewLine
+                + "A. Push the computer clock 1 hour forward into the future" + Environment.NewLine
+                + "or B. Put the computer to sleep for about three minutes and then resume" + Environment.NewLine
+                + "When you have carried out A or B, press OK", "Manual Test", MessageBoxButtons.OK);
+            if (scheduledFireTimeTestJob.ScheduledFireTimeUtc.HasValue)
+            {
+                var scheduledTimeUtc = scheduledFireTimeTestJob.ScheduledFireTimeUtc.Value;
+                var fireTimeUtc = scheduledFireTimeTestJob.FireTimeUtc;
+                var howLongAgo = fireTimeUtc - scheduledTimeUtc;                
+                Console.WriteLine("Event Scheduled for {0} Ticked at {1} with Difference of {2}", scheduledTimeUtc, fireTimeUtc, howLongAgo);
+                Assert.Greater(howLongAgo, TimeSpan.FromSeconds(30), "Scheduled Fire Time Wrong - It Should Have Been Earlier Because of the Misfire");
+            }
+            else
+            {
+                Assert.Fail("Failed To Get ScheduledFireTimeUtc at {0}", DateTimeOffset.UtcNow);
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {            
+            sched.Shutdown();            
+        }
+
+    }
+}
