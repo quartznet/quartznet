43de0dc84bdbc01d10c5a93b62cbad337fde5e8f
Marko Lahma <marko.lahma@gmail.com>
2016-06-22 22:21:54 +0300

    fix NUnit.Lite Program.cs usage

--- src/Quartz.Tests.Integration/Program.cs
+++ src/Quartz.Tests.Integration/Program.cs
@@ -7,7 +7,7 @@ namespace Quartz.Tests.Integration
         public static int Main(string[] args)
         {
 #if NETCORE
-            return new NUnitLite.AutoRun(typeof(Program).GetTypeInfo().Assembly)
+            return new NUnitLite.AutoRun(System.Reflection.IntrospectionExtensions.GetTypeInfo(typeof(Program)).Assembly)
                 .Execute(args, new NUnit.Common.ExtendedTextWrapper(Console.Out), Console.In);
 #else
             Console.WriteLine("Please run with nunit runner");
--- src/Quartz.Tests.Unit/Program.cs
+++ src/Quartz.Tests.Unit/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Reflection;
-
-using NUnit.Common;
 
 namespace Quartz.Tests.Unit
 {
@@ -10,8 +7,8 @@ namespace Quartz.Tests.Unit
         public static int Main(string[] args)
         {
 #if NETCORE
-            return new NUnitLite.AutoRun(typeof(Program).GetTypeInfo().Assembly)
-                .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+            return new NUnitLite.AutoRun(System.Reflection.IntrospectionExtensions.GetTypeInfo(typeof(Program)).Assembly)
+                .Execute(args, new NUnit.Common.ExtendedTextWrapper(Console.Out), Console.In);
 #else
             Console.WriteLine("Please run with nunit runner");
             return 0;
