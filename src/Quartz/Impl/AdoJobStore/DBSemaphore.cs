ec871a5d08a9d49ed618ec8da15e0928aa455f1b
Marko Lahma <marko.lahma@gmail.com>
2016-08-18 17:07:17 +0300

    fixes #393 DBSemaphore has wrong level and format in debug logging

--- src/Quartz/Impl/AdoJobStore/DBSemaphore.cs
+++ src/Quartz/Impl/AdoJobStore/DBSemaphore.cs
@@ -1,20 +1,20 @@
 #region License
 
-/* 
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved. 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy 
- * of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
  * under the License.
- * 
+ *
  */
 
 #endregion
@@ -31,8 +31,8 @@ using Quartz.Util;
 namespace Quartz.Impl.AdoJobStore
 {
     /// <summary>
-    /// Base class for database based lock handlers for providing thread/resource locking 
-    /// in order to protect resources from being altered by multiple threads at the 
+    /// Base class for database based lock handlers for providing thread/resource locking
+    /// in order to protect resources from being altered by multiple threads at the
     /// same time.
     /// </summary>
     /// <author>Marko Lahma (.NET)</author>
@@ -162,10 +162,8 @@ namespace Quartz.Impl.AdoJobStore
             }
             else if (log.IsDebugEnabled())
             {
-                Log.WarnFormat("Lock '{0}' attempt to return by: {1} -- but not owner!",
-                    new Exception("stack-trace of wrongful returner"),
-                    lockName,
-                    Thread.CurrentThread.Name);
+                log.DebugException($"Lock '{lockName}' attempt to return by: {Thread.CurrentThread.Name} -- but not owner!",
+                    new Exception("stack-trace of wrongful returner"));
             }
 
             return TaskUtil.CompletedTask;
