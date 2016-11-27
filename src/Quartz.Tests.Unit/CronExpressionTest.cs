d885166b0d0194eac4889437c3ba3b9912d2eed2
Marko Lahma <marko.lahma@gmail.com>
2016-11-27 11:27:18 +0200

    fixes #432 CronExpression doesn't properly check the range when an "/interval" is specified

--- src/Quartz.Tests.Unit/CronExpressionTest.cs
+++ src/Quartz.Tests.Unit/CronExpressionTest.cs
@@ -462,7 +462,7 @@ namespace Quartz.Tests.Unit
             try
             {
                 new CronExpression("0/5 * * 32W 1 ?");
-                Assert.Fail("Expected ParseException did not fire for W with value larger than 31");
+                Assert.Fail("Expected FormatException did not fire for W with value larger than 31");
             }
             catch (FormatException pe)
             {
@@ -554,6 +554,126 @@ namespace Quartz.Tests.Unit
             var actualTime = expression.GetTimeAfter(startTime);
             DateTimeOffset expected = new DateTimeOffset(2012, 11, 8, 0, 0, 0, TimeSpan.FromHours(-5));
             Assert.AreEqual(expected, actualTime);
+        }
+
+        [Test]
+        public void TestSecRangeIntervalAfterSlash()
+        {
+            // Test case 1
+            var e = Assert.Throws<FormatException>(() => new CronExpression("/120 0 8-18 ? * 2-6"), "Cron did not validate bad range interval in '_blank/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 60 : 120"));
+
+            // Test case 2
+            e = Assert.Throws<FormatException>(() => new CronExpression("0/120 0 8-18 ? * 2-6"), "Cron did not validate bad range interval in in '0/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 60 : 120"));
+
+            // Test case 3
+            e = Assert.Throws<FormatException>(() => new CronExpression("/ 0 8-18 ? * 2-6"), "Cron did not validate bad range interval in '_blank/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+
+            // Test case 4
+            e = Assert.Throws<FormatException>(() => new CronExpression("0/ 0 8-18 ? * 2-6"), "Cron did not validate bad range interval in '0/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+        }
+
+        [Test]
+        public void TestMinRangeIntervalAfterSlash()
+        {
+            // Test case 1
+            var e = Assert.Throws<FormatException>(() => new CronExpression("0 /120 8-18 ? * 2-6"), "Cron did not validate bad range interval in '_blank/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 60 : 120"));
+
+            // Test case 2
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0/120 8-18 ? * 2-6"), "Cron did not validate bad range interval in in '0/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 60 : 120"));
+
+            // Test case 3
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 / 8-18 ? * 2-6"), "Cron did not validate bad range interval in '_blank/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+
+            // Test case 4
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0/ 8-18 ? * 2-6"), "Cron did not validate bad range interval in '0/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+        }
+
+        [Test]
+        public void TestHourRangeIntervalAfterSlash()
+        {
+            // Test case 1
+            var e = Assert.Throws<FormatException>(() => new CronExpression("0 0 /120 ? * 2-6"), "Cron did not validate bad range interval in '_blank/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 24 : 120"));
+
+            // Test case 2
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0/120 ? * 2-6"), "Cron did not validate bad range interval in in '0/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 24 : 120"));
+
+            // Test case 3
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 / ? * 2-6"), "Cron did not validate bad range interval in '_blank/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+
+            // Test case 4
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0/ ? * 2-6"), "Cron did not validate bad range interval in '0/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+        }
+
+        [Test]
+        public void TestDayOfMonthRangeIntervalAfterSlash()
+        {
+            // Test case 1
+            var e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 /120 * 2-6"), "Cron did not validate bad range interval in '_blank/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 31 : 120"));
+
+            // Test case 2
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 0/120 * 2-6"), "Cron did not validate bad range interval in in '0/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 31 : 120"));
+
+            // Test case 3
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 / * 2-6"), "Cron did not validate bad range interval in '_blank/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+
+            // Test case 4
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 0/ * 2-6"), "Cron did not validate bad range interval in '0/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+        }
+
+        [Test]
+        public void TestMonthRangeIntervalAfterSlash()
+        {
+            // Test case 1
+            var e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 ? /120 2-6"), "Cron did not validate bad range interval in '_blank/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 12 : 120"));
+
+            // Test case 2
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 ? 0/120 2-6"), "Cron did not validate bad range interval in in '0/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 12 : 120"));
+
+            // Test case 3
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 ? / 2-6"), "Cron did not validate bad range interval in '_blank/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+
+            // Test case 4
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 ? 0/ 2-6"), "Cron did not validate bad range interval in '0/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+        }
+
+        [Test]
+        public void TestDayOfWeekRangeIntervalAfterSlash()
+        {
+            // Test case 1
+            var e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 ? * /120"), "Cron did not validate bad range interval in '_blank/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 7 : 120"));
+
+            // Test case 2
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 ? * 0/120"), "Cron did not validate bad range interval in in '0/xxx' form");
+            Assert.That(e.Message, Is.EqualTo("Increment > 7 : 120"));
+
+            // Test case 3
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 ? * /"), "Cron did not validate bad range interval in '_blank/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
+
+            // Test case 4
+            e = Assert.Throws<FormatException>(() => new CronExpression("0 0 0 ? * 0/"), "Cron did not validate bad range interval in '0/_blank'");
+            Assert.That(e.Message, Is.EqualTo("'/' must be followed by an integer."));
         }
 
         private class SimpleCronExpression : CronExpression
--- src/Quartz/CronExpression.cs
+++ src/Quartz/CronExpression.cs
@@ -1,19 +1,19 @@
 #region License
-/* 
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved. 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy 
- * of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
  * under the License.
- * 
+ *
  */
 #endregion
 
@@ -31,10 +31,10 @@ using Quartz.Util;
 namespace Quartz
 {
     /// <summary>
-    /// Provides a parser and evaluator for unix-like cron expressions. Cron 
+    /// Provides a parser and evaluator for unix-like cron expressions. Cron
     /// expressions provide the ability to specify complex time combinations such as
-    /// &quot;At 8:00am every Monday through Friday&quot; or &quot;At 1:30am every 
-    /// last Friday of the month&quot;. 
+    /// &quot;At 8:00am every Monday through Friday&quot; or &quot;At 1:30am every
+    /// last Friday of the month&quot;.
     /// </summary>
     /// <remarks>
     /// <para>
@@ -100,7 +100,7 @@ namespace Quartz
     /// </tr>
     /// </table>
     /// <para>
-    /// The '*' character is used to specify all values. For example, &quot;*&quot; 
+    /// The '*' character is used to specify all values. For example, &quot;*&quot;
     /// in the minute field means &quot;every minute&quot;.
     /// </para>
     /// <para>
@@ -119,59 +119,59 @@ namespace Quartz
     /// </para>
     /// <para>
     /// The '/' character is used to specify increments. For example &quot;0/15&quot;
-    /// in the seconds field means &quot;the seconds 0, 15, 30, and 45&quot;. And 
+    /// in the seconds field means &quot;the seconds 0, 15, 30, and 45&quot;. And
     /// &quot;5/15&quot; in the seconds field means &quot;the seconds 5, 20, 35, and
     /// 50&quot;.  Specifying '*' before the  '/' is equivalent to specifying 0 is
     /// the value to start with. Essentially, for each field in the expression, there
-    /// is a set of numbers that can be turned on or off. For seconds and minutes, 
+    /// is a set of numbers that can be turned on or off. For seconds and minutes,
     /// the numbers range from 0 to 59. For hours 0 to 23, for days of the month 0 to
     /// 31, and for months 1 to 12. The &quot;/&quot; character simply helps you turn
     /// on every &quot;nth&quot; value in the given set. Thus &quot;7/6&quot; in the
-    /// month field only turns on month &quot;7&quot;, it does NOT mean every 6th 
-    /// month, please note that subtlety.  
+    /// month field only turns on month &quot;7&quot;, it does NOT mean every 6th
+    /// month, please note that subtlety.
     /// </para>
     /// <para>
     /// The 'L' character is allowed for the day-of-month and day-of-week fields.
-    /// This character is short-hand for &quot;last&quot;, but it has different 
-    /// meaning in each of the two fields. For example, the value &quot;L&quot; in 
-    /// the day-of-month field means &quot;the last day of the month&quot; - day 31 
-    /// for January, day 28 for February on non-leap years. If used in the 
-    /// day-of-week field by itself, it simply means &quot;7&quot; or 
+    /// This character is short-hand for &quot;last&quot;, but it has different
+    /// meaning in each of the two fields. For example, the value &quot;L&quot; in
+    /// the day-of-month field means &quot;the last day of the month&quot; - day 31
+    /// for January, day 28 for February on non-leap years. If used in the
+    /// day-of-week field by itself, it simply means &quot;7&quot; or
     /// &quot;SAT&quot;. But if used in the day-of-week field after another value, it
     /// means &quot;the last xxx day of the month&quot; - for example &quot;6L&quot;
-    /// means &quot;the last friday of the month&quot;. You can also specify an offset 
-    /// from the last day of the month, such as "L-3" which would mean the third-to-last 
-    /// day of the calendar month. <i>When using the 'L' option, it is important not to 
+    /// means &quot;the last friday of the month&quot;. You can also specify an offset
+    /// from the last day of the month, such as "L-3" which would mean the third-to-last
+    /// day of the calendar month. <i>When using the 'L' option, it is important not to
     /// specify lists, or ranges of values, as you'll get confusing/unexpected results.</i>
     /// </para>
     /// <para>
-    /// The 'W' character is allowed for the day-of-month field.  This character 
-    /// is used to specify the weekday (Monday-Friday) nearest the given day.  As an 
-    /// example, if you were to specify &quot;15W&quot; as the value for the 
+    /// The 'W' character is allowed for the day-of-month field.  This character
+    /// is used to specify the weekday (Monday-Friday) nearest the given day.  As an
+    /// example, if you were to specify &quot;15W&quot; as the value for the
     /// day-of-month field, the meaning is: &quot;the nearest weekday to the 15th of
-    /// the month&quot;. So if the 15th is a Saturday, the trigger will fire on 
+    /// the month&quot;. So if the 15th is a Saturday, the trigger will fire on
     /// Friday the 14th. If the 15th is a Sunday, the trigger will fire on Monday the
-    /// 16th. If the 15th is a Tuesday, then it will fire on Tuesday the 15th. 
+    /// 16th. If the 15th is a Tuesday, then it will fire on Tuesday the 15th.
     /// However if you specify &quot;1W&quot; as the value for day-of-month, and the
-    /// 1st is a Saturday, the trigger will fire on Monday the 3rd, as it will not 
-    /// 'jump' over the boundary of a month's days.  The 'W' character can only be 
+    /// 1st is a Saturday, the trigger will fire on Monday the 3rd, as it will not
+    /// 'jump' over the boundary of a month's days.  The 'W' character can only be
     /// specified when the day-of-month is a single day, not a range or list of days.
     /// </para>
     /// <para>
-    /// The 'L' and 'W' characters can also be combined for the day-of-month 
-    /// expression to yield 'LW', which translates to &quot;last weekday of the 
+    /// The 'L' and 'W' characters can also be combined for the day-of-month
+    /// expression to yield 'LW', which translates to &quot;last weekday of the
     /// month&quot;.
     /// </para>
     /// <para>
     /// The '#' character is allowed for the day-of-week field. This character is
-    /// used to specify &quot;the nth&quot; XXX day of the month. For example, the 
-    /// value of &quot;6#3&quot; in the day-of-week field means the third Friday of 
-    /// the month (day 6 = Friday and &quot;#3&quot; = the 3rd one in the month). 
-    /// Other examples: &quot;2#1&quot; = the first Monday of the month and 
+    /// used to specify &quot;the nth&quot; XXX day of the month. For example, the
+    /// value of &quot;6#3&quot; in the day-of-week field means the third Friday of
+    /// the month (day 6 = Friday and &quot;#3&quot; = the 3rd one in the month).
+    /// Other examples: &quot;2#1&quot; = the first Monday of the month and
     /// &quot;4#5&quot; = the fifth Wednesday of the month. Note that if you specify
     /// &quot;#5&quot; and there is not 5 of the given day-of-week in the month, then
     /// no firing will occur that month. If the '#' character is used, there can
-    /// only be one expression in the day-of-week field (&quot;3#1,6#3&quot; is 
+    /// only be one expression in the day-of-week field (&quot;3#1,6#3&quot; is
     /// not valid, since there are two expressions).
     /// </para>
     /// <para>
@@ -193,12 +193,12 @@ namespace Quartz
     /// <li>Support for specifying both a day-of-week and a day-of-month value is
     /// not complete (you'll need to use the '?' character in one of these fields).
     /// </li>
-    /// <li>Overflowing ranges is supported - that is, having a larger number on 
-    /// the left hand side than the right. You might do 22-2 to catch 10 o'clock 
-    /// at night until 2 o'clock in the morning, or you might have NOV-FEB. It is 
-    /// very important to note that overuse of overflowing ranges creates ranges 
-    /// that don't make sense and no effort has been made to determine which 
-    /// interpretation CronExpression chooses. An example would be 
+    /// <li>Overflowing ranges is supported - that is, having a larger number on
+    /// the left hand side than the right. You might do 22-2 to catch 10 o'clock
+    /// at night until 2 o'clock in the morning, or you might have NOV-FEB. It is
+    /// very important to note that overuse of overflowing ranges creates ranges
+    /// that don't make sense and no effort has been made to determine which
+    /// interpretation CronExpression chooses. An example would be
     /// "0 0 14-6 ? * FRI-MON". </li>
     /// </ul>
     /// </para>
@@ -225,47 +225,47 @@ namespace Quartz
 		/// Field specification for minute.
 		/// </summary>
 		protected const int Minute = 1;
-        
+
 		/// <summary>
 		/// Field specification for hour.
 		/// </summary>
 		protected const int Hour = 2;
-        
+
 		/// <summary>
 		/// Field specification for day of month.
 		/// </summary>
 		protected const int DayOfMonth = 3;
-        
+
 		/// <summary>
 		/// Field specification for month.
 		/// </summary>
 		protected const int Month = 4;
-        
+
 		/// <summary>
 		/// Field specification for day of week.
 		/// </summary>
 		protected const int DayOfWeek = 5;
-        
+
 		/// <summary>
 		/// Field specification for year.
 		/// </summary>
 		protected const int Year = 6;
-        
+
 		/// <summary>
 		/// Field specification for all wildcard value '*'.
 		/// </summary>
 		protected const int AllSpecInt = 99; // '*'
-        
+
 		/// <summary>
 		/// Field specification for not specified value '?'.
 		/// </summary>
 		protected const int NoSpecInt = 98; // '?'
-        
+
 		/// <summary>
 		/// Field specification for wildcard '*'.
 		/// </summary>
 		protected const int AllSpec = AllSpecInt;
-		
+
 		/// <summary>
 		/// Field specification for no specification at all '?'.
 		/// </summary>
@@ -426,7 +426,7 @@ namespace Quartz
         }
 
         ///<summary>
-        /// Constructs a new <see cref="CronExpressionString" /> based on the specified 
+        /// Constructs a new <see cref="CronExpressionString" /> based on the specified
         /// parameter.
         /// </summary>
         /// <param name="cronExpression">
@@ -445,7 +445,7 @@ namespace Quartz
         }
 
         /// <summary>
-        /// Indicates whether the given date satisfies the cron expression. 
+        /// Indicates whether the given date satisfies the cron expression.
         /// </summary>
         /// <remarks>
         /// Note that  milliseconds are ignored, so two Dates falling on different milliseconds
@@ -463,7 +463,7 @@ namespace Quartz
             {
                 return true;
             }
-            
+
             return false;
         }
 
@@ -545,7 +545,7 @@ namespace Quartz
         }
 
         /// <summary>
-        /// Indicates whether the specified cron expression can be parsed into a 
+        /// Indicates whether the specified cron expression can be parsed into a
         /// valid cron expression
         /// </summary>
         /// <param name="cronExpression">the expression to evaluate</param>
@@ -565,7 +565,7 @@ namespace Quartz
             return true;
         }
 
-        
+
         public static void ValidateExpression(string cronExpression)
         {
             new CronExpression(cronExpression);
@@ -861,31 +861,8 @@ namespace Quartz
                     {
                         i++;
                     }
-                    if (incr > 59 && (type == Second || type == Minute))
-                    {
-                        throw new FormatException(
-                            $"Increment > 60 : {incr}");
+                    CheckIncrementRange(incr, type);
                     }
-                    else if (incr > 23 && (type == Hour))
-                    {
-                        throw new FormatException(
-                            $"Increment > 24 : {incr}");
-                    }
-                    else if (incr > 31 && (type == DayOfMonth))
-                    {
-                        throw new FormatException(
-                            $"Increment > 31 : {incr}");
-                    }
-                    else if (incr > 7 && (type == DayOfWeek))
-                    {
-                        throw new FormatException(
-                            $"Increment > 7 : {incr}");
-                    }
-                    else if (incr > 12 && (type == Month))
-                    {
-                        throw new FormatException($"Increment > 12 : {incr}");
-                    }
-                }
                 else
                 {
                     incr = 1;
@@ -959,6 +936,30 @@ namespace Quartz
             return i;
         }
 
+        private void CheckIncrementRange(int incr, int type)
+        {
+            if (incr > 59 && (type == Second || type == Minute))
+            {
+                throw new FormatException($"Increment > 60 : {incr}");
+            }
+            if (incr > 23 && type == Hour)
+            {
+                throw new FormatException($"Increment > 24 : {incr}");
+            }
+            if (incr > 31 && type == DayOfMonth)
+            {
+                throw new FormatException($"Increment > 31 : {incr}");
+            }
+            if (incr > 7 && type == DayOfWeek)
+            {
+                throw new FormatException($"Increment > 7 : {incr}");
+            }
+            if (incr > 12 && type == Month)
+            {
+                throw new FormatException($"Increment > 12 : {incr}");
+            }
+        }
+
 		/// <summary>
 		/// Checks the next value.
 		/// </summary>
@@ -1013,7 +1014,7 @@ namespace Quartz
                 if (val > 31)
                 {
                     throw new FormatException("The 'W' option does not make sense with values larger than 31 (max number of days in a month)");
-                } 
+                }
 
                 ISet<int> data = GetSet(type);
                 data.Add(val);
@@ -1124,12 +1125,18 @@ namespace Quartz
 
             if (c == '/')
             {
+                if ((i + 1) >= s.Length || s[i + 1] == ' ' || s[i + 1] == '\t')
+                {
+                    throw new FormatException("\'/\' must be followed by an integer.");
+                }
+
                 i++;
                 c = s[i];
                 int v2 = Convert.ToInt32(c.ToString(), CultureInfo.InvariantCulture);
                 i++;
                 if (i >= s.Length)
                 {
+                    CheckIncrementRange(v2, type);
                     AddToSet(val, end, v2, type);
                     return i;
                 }
@@ -1138,6 +1145,7 @@ namespace Quartz
                 {
                     ValueSet vs = GetValue(v2, s, i);
                     int v3 = vs.theValue;
+                    CheckIncrementRange(v3, type);
                     AddToSet(val, end, v3, type);
                     i = vs.pos;
                     return i;
@@ -1419,8 +1427,8 @@ namespace Quartz
                 }
             }
 
-            // if the end of the range is before the start, then we need to overflow into 
-            // the next day, month etc. This is done by adding the maximum amount for that 
+            // if the end of the range is before the start, then we need to overflow into
+            // the next day, month etc. This is done by adding the maximum amount for that
             // type, and using modulus max to determine the value being added.
             int max = -1;
             if (stopAt < startAt)
@@ -1548,7 +1556,7 @@ namespace Quartz
             {
                 return monthMap[s];
             }
-            
+
             return -1;
         }
 
@@ -1563,7 +1571,7 @@ namespace Quartz
             {
                 return dayMap[s];
             }
-            
+
             return -1;
         }
 
@@ -1835,7 +1843,7 @@ namespace Quartz
                             // This is to avoid a bug when moving from a month
                             //with 30 or 31 days to a month with less. Causes an invalid datetime to be instantiated.
                             // ex. 0 29 0 30 1 ? 2009 with clock set to 1/30/2009
-                            int lDay = DateTime.DaysInMonth(d.Year, mon); 
+                            int lDay = DateTime.DaysInMonth(d.Year, mon);
                             if (day <= lDay)
                             {
                                 d = new DateTimeOffset(d.Year, mon, day, 0, 0, 0, d.Offset);
@@ -1843,7 +1851,7 @@ namespace Quartz
                             else
                             {
                                 d = new DateTimeOffset(d.Year, mon, lDay, 0, 0, 0, d.Offset).AddDays(day - lDay);
-                            } 
+                        }
                         }
                         continue;
                     }
@@ -1938,8 +1946,8 @@ namespace Quartz
                             else
                             {
                                 d = new DateTimeOffset(d.Year, mon + 1, 1, 0, 0, 0, d.Offset);
-                            } 
-                            
+                            }
+
                             // we are promoting the month
                             continue;
                         }
@@ -2113,7 +2121,7 @@ namespace Quartz
         }
 
         /// <summary>
-        /// NOT YET IMPLEMENTED: Returns the final time that the 
+        /// NOT YET IMPLEMENTED: Returns the final time that the
         /// <see cref="CronExpression" /> will match.
         /// </summary>
         /// <returns></returns>
@@ -2203,7 +2211,7 @@ namespace Quartz
         }
 
         /// <summary>
-        /// Serves as a hash function for a particular type. 
+        /// Serves as a hash function for a particular type.
         /// </summary>
         /// <returns>
         /// A hash code for the current <see cref="T:System.Object"/>.
