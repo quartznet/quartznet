6c2583b21a23b1256342eb4efb83dcffb676d1aa
Marko Lahma <marko.lahma@gmail.com>
2016-11-27 11:52:16 +0200

    fixes #433 RAMJobStore fails to resume when paused without triggers

---
+++ src/Quartz.Tests.Integration/QuartzMemoryPauseAndResumeTest.cs
@@ -0,0 +1,149 @@
+﻿using NUnit.Framework;
+
+using Quartz.Impl;
+using Quartz.Impl.Matchers;
+using Quartz.Job;
+
+namespace Quartz.Tests.Integration
+{
+    [TestFixture]
+    public class QuartzMemoryPauseAndResumeTest
+    {
+        private IScheduler scheduler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            ISchedulerFactory sf = new StdSchedulerFactory();
+            scheduler = sf.GetScheduler();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            scheduler.Shutdown(true);
+        }
+
+        [Test]
+        public void TestPauseAndResumeTriggers()
+        {
+            IJobDetail jobDetail = JobBuilder.Create<NoOpJob>()
+                .WithIdentity("test")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("test", "abc")
+                .WithCronSchedule("* * * * * ?")
+                .Build();
+
+            scheduler.ScheduleJob(jobDetail, trigger);
+
+            TriggerState state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Normal));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Paused));
+
+            scheduler.PauseTriggers(GroupMatcher<TriggerKey>.GroupEquals("abc"));
+            state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Paused));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Normal));
+
+            scheduler.ResumeTriggers(GroupMatcher<TriggerKey>.GroupEquals("abc"));
+            state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Normal));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Paused));
+        }
+
+        [Test]
+        public void TestResumeTriggersBeforeAddJob()
+        {
+            scheduler.PauseTriggers(GroupMatcher<TriggerKey>.GroupEquals("abc"));
+            scheduler.ResumeTriggers(GroupMatcher<TriggerKey>.GroupEquals("abc"));
+
+            IJobDetail jobDetail = JobBuilder.Create<NoOpJob>()
+                .WithIdentity("test")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("test", "abc")
+                .WithCronSchedule("* * * * * ?")
+                .Build();
+
+            scheduler.ScheduleJob(jobDetail, trigger);
+
+            TriggerState state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Normal));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Paused));
+
+            scheduler.PauseTriggers(GroupMatcher<TriggerKey>.GroupEquals("abc"));
+            state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Paused));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Normal));
+
+            scheduler.ResumeTriggers(GroupMatcher<TriggerKey>.GroupEquals("abc"));
+            state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Normal));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Paused));
+        }
+
+        [Test]
+        public void TestPauseAndResumeJobs()
+        {
+            IJobDetail jobDetail = JobBuilder.Create<NoOpJob>()
+                .WithIdentity("test", "abc")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("test", "abc")
+                .WithCronSchedule("* * * * * ?")
+                .Build();
+
+            scheduler.ScheduleJob(jobDetail, trigger);
+
+            TriggerState state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Normal));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Paused));
+
+            scheduler.PauseJobs(GroupMatcher<JobKey>.GroupEquals("abc"));
+            state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Paused));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Normal));
+
+            scheduler.ResumeJobs(GroupMatcher<JobKey>.GroupEquals("abc"));
+            state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Normal));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Paused));
+        }
+
+        [Test]
+        public void TestResumeJobsBeforeAddJobs()
+        {
+            scheduler.PauseJobs(GroupMatcher<JobKey>.GroupEquals("abc"));
+            scheduler.ResumeJobs(GroupMatcher<JobKey>.GroupEquals("abc"));
+
+            IJobDetail jobDetail = JobBuilder.Create<NoOpJob>()
+                .WithIdentity("test", "abc")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("test", "abc")
+                .WithCronSchedule("* * * * * ?")
+                .Build();
+
+            scheduler.ScheduleJob(jobDetail, trigger);
+
+            TriggerState state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Normal));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Paused));
+
+            scheduler.PauseJobs(GroupMatcher<JobKey>.GroupEquals("abc"));
+            state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Paused));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Normal));
+
+            scheduler.ResumeJobs(GroupMatcher<JobKey>.GroupEquals("abc"));
+            state = scheduler.GetTriggerState(new TriggerKey("test", "abc"));
+            Assert.That(state, Is.EqualTo(TriggerState.Normal));
+            Assert.That(state, Is.Not.EqualTo(TriggerState.Paused));
+        }
+    }
+}
--- src/Quartz/Simpl/RAMJobStore.cs
+++ src/Quartz/Simpl/RAMJobStore.cs
@@ -1313,12 +1313,32 @@ namespace Quartz.Simpl
                     }
                     ResumeTrigger(triggerKey);
                 }
-                foreach (String group in groups)
+                // Find all matching paused trigger groups, and then remove them.
+                StringOperator op = matcher.CompareWithOperator;
+                var pausedGroups = new List<string>();
+                var matcherGroup = matcher.CompareToValue;
+                if (op.Equals(StringOperator.Equality))
                 {
-                    pausedTriggerGroups.Remove(group);
+                    if (pausedTriggerGroups.Contains(matcherGroup))
+                    {
+                        pausedGroups.Add(matcher.CompareToValue);
+                    }
+                }
+                else
+                {
+                    foreach (string group in pausedTriggerGroups)
+                    {
+                        if (op.Evaluate(group, matcherGroup))
+                        {
+                            pausedGroups.Add(group);
+                        }
+                    }
+                }
+                foreach (string pausedGroup in pausedGroups)
+                {
+                    pausedTriggerGroups.Remove(pausedGroup);
                 }
             }
-
             return new List<string>(groups);
         }
 
