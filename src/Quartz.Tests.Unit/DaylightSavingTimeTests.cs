c3d7ea2697fdb5106df1cba63ed68ec77a4ab19b
Matt Johnson <mj1856@hotmail.com>
2016-12-19 14:44:41 -0800

    Fix current date for testing

--- src/Quartz.Tests.Unit/DaylightSavingTimeTests.cs
+++ src/Quartz.Tests.Unit/DaylightSavingTimeTests.cs
@@ -1,19 +1,19 @@
 ﻿#region License
-/* 
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved. 
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
- * use this file except in compliance with the License. You may obtain a copy 
- * of the License at 
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0 
- *   
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations 
+/*
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
  * under the License.
- * 
+ *
  */
 #endregion
 
@@ -26,6 +26,21 @@ namespace Quartz.Tests.Unit
     [TestFixture]
     public class DaylightSavingTimeTest
     {
+        private Func<DateTimeOffset> OriginalUtcNow;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            OriginalUtcNow = SystemTime.UtcNow;
+            SystemTime.UtcNow = () => new DateTimeOffset(2016, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        }
+
+        [TestFixtureTearDown]
+        public void Dispose()
+        {
+            SystemTime.UtcNow = OriginalUtcNow;
+        }
+
         [Test]
         public void ShouldHandleDstSpringForwardTransition()
         {
