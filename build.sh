66db1b3429216912ed923430cc43f509912c5dd7
Marko Lahma <marko.lahma@gmail.com>
2017-07-09 12:37:49 +0300

    fixes #486 add support for .NET Standard 2.0

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FAKE_VERSION="4.61.3"
+FAKE_VERSION="4.62.1"
 
 mono tools/NuGet/NuGet.exe install FAKE -Version $FAKE_VERSION -OutputDirectory packages -ExcludeVersion
 mono packages/FAKE/tools/FAKE.exe build.fsx $@
