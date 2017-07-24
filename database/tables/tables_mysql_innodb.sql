249928ab220c788c243bf4a097da4aa778bcbfaf
Marko Lahma <marko.lahma@gmail.com>
2017-07-24 10:40:53 +0300

    closes #314 add example of MySQL's UTF-8 create database

--- database/tables/tables_mysql_innodb.sql
+++ database/tables/tables_mysql_innodb.sql
@@ -1,6 +1,10 @@
 # By: Ron Cordell - roncordell
 #  I didn't see this anywhere, so I thought I'd post it here. This is the script from Quartz to create the tables in a MySQL database, modified to use INNODB instead of MYISAM.
 
+
+# make sure you have UTF-8 collaction for best .NET interoperability
+# CREATE DATABASE quartznet CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 DROP TABLE IF EXISTS QRTZ_FIRED_TRIGGERS;
 DROP TABLE IF EXISTS QRTZ_PAUSED_TRIGGER_GRPS;
 DROP TABLE IF EXISTS QRTZ_SCHEDULER_STATE;
