03340367a8fbce6d3666bd4965060306f7b223d9
Marko Lahma <marko.lahma@gmail.com>
2017-10-01 19:11:32 +0300

    update copyright info

--- src/Quartz.Tests.Unit/DaylightSavingTimeTests.cs
+++ src/Quartz.Tests.Unit/DaylightSavingTimeTests.cs
@@ -1,6 +1,6 @@
 ﻿#region License
 /* 
- * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved. 
+ * All content copyright Marko Lahma, unless otherwise indicated. All rights reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
  * use this file except in compliance with the License. You may obtain a copy 
