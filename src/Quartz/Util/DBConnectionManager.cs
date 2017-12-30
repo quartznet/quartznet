7efe5cff18528da589da774ad2d10db6030aee8a
Marko Lahma <marko.lahma@gmail.com>
2017-12-30 13:25:46 +0200

    fixes #560 add lock to db provider updates

--- src/Quartz/Util/DBConnectionManager.cs
+++ src/Quartz/Util/DBConnectionManager.cs
@@ -28,44 +28,44 @@ using Quartz.Impl.AdoJobStore.Common;
 
 namespace Quartz.Util
 {
-	/// <summary>
-	/// Manages a collection of IDbProviders, and provides transparent access
-	/// to their database.
-	/// </summary>
-	/// <seealso cref="IDbProvider" />
-	/// <author>James House</author>
-	/// <author>Sharada Jambula</author>
-	/// <author>Mohammad Rezaei</author>
+    /// <summary>
+    /// Manages a collection of IDbProviders, and provides transparent access
+    /// to their database.
+    /// </summary>
+    /// <seealso cref="IDbProvider" />
+    /// <author>James House</author>
+    /// <author>Sharada Jambula</author>
+    /// <author>Mohammad Rezaei</author>
     /// <author>Marko Lahma (.NET)</author>
     public class DBConnectionManager : IDbConnectionManager
-	{        
+    {
         private static readonly DBConnectionManager instance = new DBConnectionManager();
-	    private static readonly ILog log = LogManager.GetLogger(typeof (DBConnectionManager));
+        private static readonly ILog log = LogManager.GetLogger(typeof(DBConnectionManager));
 
+        private readonly object syncRoot = new object();
         private readonly Dictionary<string, IDbProvider> providers = new Dictionary<string, IDbProvider>();
 
-		/// <summary> 
-		/// Get the class instance.
-		/// </summary>
-		/// <returns> an instance of this class
-		/// </returns>
-		public static IDbConnectionManager Instance
-		{
-			get
-			{
-				// since the instance variable is initialized at class loading time,
-				// it's not necessary to synchronize this method */
-				return instance;
-			}
-		}
+        /// <summary>
+        /// Get the class instance.
+        /// </summary>
+        /// <returns> an instance of this class
+        /// </returns>
+        public static IDbConnectionManager Instance
+        {
+            get
+            {
+                // since the instance variable is initialized at class loading time,
+                // it's not necessary to synchronize this method */
+                return instance;
+            }
+        }
 
-
-		/// <summary> 
-		/// Private constructor
-		/// </summary>
-		private DBConnectionManager()
-		{
-		}
+        /// <summary>
+        /// Private constructor
+        /// </summary>
+        private DBConnectionManager()
+        {
+        }
 
         /// <summary>
         /// Adds the connection provider.
@@ -73,34 +73,38 @@ namespace Quartz.Util
         /// <param name="dataSourceName">Name of the data source.</param>
         /// <param name="provider">The provider.</param>
         public virtual void AddConnectionProvider(string dataSourceName, IDbProvider provider)
-		{
+        {
             log.Info(string.Format("Registering datasource '{0}' with db provider: '{1}'", dataSourceName, provider));
-			providers[dataSourceName] = provider;
-		}
 
-		/// <summary>
-		/// Get a database connection from the DataSource with the given name.
-		/// </summary>
-		/// <returns> a database connection </returns>
+            lock (syncRoot)
+            {
+                providers[dataSourceName] = provider;
+            }
+        }
+
+        /// <summary>
+        /// Get a database connection from the DataSource with the given name.
+        /// </summary>
+        /// <returns> a database connection </returns>
         public virtual IDbConnection GetConnection(string dataSourceName)
-		{
+        {
             IDbProvider provider = GetDbProvider(dataSourceName);
 
-			return provider.CreateConnection();
-		}
+            return provider.CreateConnection();
+        }
 
-		/// <summary> 
-		/// Shuts down database connections from the DataSource with the given name,
-		/// if applicable for the underlying provider.
-		/// </summary>
-		public virtual void Shutdown(string dsName)
-		{
-		    IDbProvider provider = GetDbProvider(dsName);
-			provider.Shutdown();
-		}
+        /// <summary>
+        /// Shuts down database connections from the DataSource with the given name,
+        /// if applicable for the underlying provider.
+        /// </summary>
+        public virtual void Shutdown(string dsName)
+        {
+            IDbProvider provider = GetDbProvider(dsName);
+            provider.Shutdown();
+        }
 
-	    public DbMetadata GetDbMetadata(string dsName)
-	    {
+        public DbMetadata GetDbMetadata(string dsName)
+        {
             return GetDbProvider(dsName).Metadata;
         }
 
@@ -109,20 +113,25 @@ namespace Quartz.Util
         /// </summary>
         /// <param name="dsName">Name of the ds.</param>
         /// <returns></returns>
-	    public IDbProvider GetDbProvider(string dsName)
-	    {
+        public IDbProvider GetDbProvider(string dsName)
+        {
             if (String.IsNullOrEmpty(dsName))
             {
                 throw new ArgumentException("DataSource name cannot be null or empty", "dsName");
             }
 
             IDbProvider provider;
-            providers.TryGetValue(dsName, out provider);
+            lock (syncRoot)
+            {
+                providers.TryGetValue(dsName, out provider);
+            }
+
             if (provider == null)
             {
                 throw new Exception(string.Format(CultureInfo.InvariantCulture, "There is no DataSource named '{0}'", dsName));
             }
+
             return provider;
         }
-	}
+    }
 }
