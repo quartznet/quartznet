2c247e5c04f0548bca0e384f70649421da7ea53f
puddlewitt <puddlewitt@hotmail.com>
2018-09-21 16:50:48 +0100

    Moved impl to JobStoreTX not base.

--- src/Quartz/Impl/AdoJobStore/JobStoreSupport.cs
+++ src/Quartz/Impl/AdoJobStore/JobStoreSupport.cs
@@ -78,7 +78,7 @@ namespace Quartz.Impl.AdoJobStore
             MaxMisfiresToHandleAtATime = 20;
             DbRetryInterval = TimeSpan.FromSeconds(15);
             Log = LogProvider.GetLogger(GetType());
-            delegateType = typeof (StdAdoDelegate);
+            delegateType = typeof(StdAdoDelegate);
         }
 
         /// <summary>
@@ -359,7 +359,7 @@ namespace Quartz.Impl.AdoJobStore
                 DateTimeOffset misfireTime = SystemTime.UtcNow();
                 if (MisfireThreshold > TimeSpan.Zero)
                 {
-                    misfireTime = misfireTime.AddMilliseconds(-1*MisfireThreshold.TotalMilliseconds);
+                    misfireTime = misfireTime.AddMilliseconds(-1 * MisfireThreshold.TotalMilliseconds);
                 }
 
                 return misfireTime;
@@ -407,7 +407,7 @@ namespace Quartz.Impl.AdoJobStore
                                 throw new InvalidConfigurationException("Configured delegate does not have public constructor that takes no arguments");
                             }
 
-                            driverDelegate = (IDriverDelegate) ctor.Invoke(null);
+                            driverDelegate = (IDriverDelegate)ctor.Invoke(null);
                             driverDelegate.Initialize(args);
                         }
                         catch (Exception e)
@@ -516,7 +516,7 @@ namespace Quartz.Impl.AdoJobStore
                     Log.Warn("Detected usage of SqlServerDelegate and UpdateLockRowSemaphore, removing 'quartz.jobStore.lockHandler.type' would allow more efficient SQL Server specific (UPDLOCK,ROWLOCK) row access");
                 }
                 // be ready to give a friendly warning if SQL Server provider and wrong delegate
-                if (DbProvider != null && DbProvider.Metadata.ConnectionType == typeof (SqlConnection) && !(Delegate is SqlServerDelegate))
+                if (DbProvider != null && DbProvider.Metadata.ConnectionType == typeof(SqlConnection) && !(Delegate is SqlServerDelegate))
                 {
                     Log.Warn("Detected usage of SQL Server provider without SqlServerDelegate, SqlServerDelegate would provide better performance");
                 }
@@ -772,7 +772,7 @@ namespace Quartz.Impl.AdoJobStore
                 DateTimeOffset misfireTime = SystemTime.UtcNow();
                 if (MisfireThreshold > TimeSpan.Zero)
                 {
-                    misfireTime = misfireTime.AddMilliseconds(-1*MisfireThreshold.TotalMilliseconds);
+                    misfireTime = misfireTime.AddMilliseconds(-1 * MisfireThreshold.TotalMilliseconds);
                 }
 
                 if (trig.GetNextFireTimeUtc().GetValueOrDefault() > misfireTime)
@@ -1147,7 +1147,7 @@ namespace Quartz.Impl.AdoJobStore
                         await StoreJob(conn, job, replace, cancellationToken).ConfigureAwait(false);
                         foreach (var trigger in triggers)
                         {
-                            await StoreTrigger(conn, (IOperableTrigger) trigger, job, replace, StateWaiting, false, false, cancellationToken).ConfigureAwait(false);
+                            await StoreTrigger(conn, (IOperableTrigger)trigger, job, replace, StateWaiting, false, false, cancellationToken).ConfigureAwait(false);
                         }
                     }
                 }, cancellationToken).ConfigureAwait(false);
@@ -3479,156 +3479,23 @@ namespace Quartz.Impl.AdoJobStore
             cth.Rollback(IsTransient(cause));
         }
 
-
-        /// <summary>
-        /// Taken from https://github.com/aspnet/EntityFrameworkCore/blob/d59be61006d78d507dea07a9779c3c4103821ca3/src/EFCore.SqlServer/Storage/Internal/SqlServerTransientExceptionDetector.cs
-        /// and merged with https://docs.microsoft.com/en-us/azure/sql-database/sql-database-develop-error-messages
-        ///
-        /// Copied from EFCore because it states "not intended to be used directly from your code" and we don't
-        /// want EF leaking into Quartz.
-        /// </summary>
-        /// <param name="ex"></param>
-        /// <returns></returns>
         protected virtual bool IsTransient(Exception ex)
         {
-            var sqlException = ex as SqlException ?? ex?.InnerException as SqlException;
-
+            SqlException sqlException = ex as SqlException ?? ex?.InnerException as SqlException;
             if (sqlException != null)
             {
-                // https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlexception?view=netframework-4.7.2
-                // "SqlException always contains at least one instance of SqlError"
-                // SQlException.Number "which is a wrapper for the Number property of the first SqlError in the Errors property."
-
-                foreach (SqlError err in sqlException.Errors)
+                switch (sqlException.Number)
                 {
-                    Log.Info("SqlError " + err);
-
-                    switch (err.Number)
-                    {
-                        // SQL Error Code: 49920
-                        // Cannot process request. Too many operations in progress for subscription "%ld".
-                        // The service is busy processing multiple requests for this subscription.
-                        // Requests are currently blocked for resource optimization. Query sys.dm_operation_status for operation status.
-                        // Wait until pending requests are complete or delete one of your pending requests and retry your request later.
-                        case 49920:
-                        // SQL Error Code: 49919
-                        // Cannot process create or update request. Too many create or update operations in progress for subscription "%ld".
-                        // The service is busy processing multiple create or update requests for your subscription or server.
-                        // Requests are currently blocked for resource optimization. Query sys.dm_operation_status for pending operations.
-                        // Wait till pending create or update requests are complete or delete one of your pending requests and
-                        // retry your request later.
-                        case 49919:
-                        // SQL Error Code: 49918
-                        // Cannot process request. Not enough resources to process request.
-                        // The service is currently busy.Please retry the request later.
-                        case 49918:
-                        // SQL Error Code: 41839
-                        // Transaction exceeded the maximum number of commit dependencies.
-                        case 41839:
-                        // SQL Error Code: 41325
-                        // The current transaction failed to commit due to a serializable validation failure.
-                        case 41325:
-                        // SQL Error Code: 41305
-                        // The current transaction failed to commit due to a repeatable read validation failure.
-                        case 41305:
-                        // SQL Error Code: 41302
-                        // The current transaction attempted to update a record that has been updated since the transaction started.
-                        case 41302:
-                        // SQL Error Code: 41301
-                        // Dependency failure: a dependency was taken on another transaction that later failed to commit.
-                        case 41301:
-                        // SQL Error Code: 40613
-                        // Database XXXX on server YYYY is not currently available. Please retry the connection later.
-                        // If the problem persists, contact customer support, and provide them the session tracing ID of ZZZZZ.
-                        case 40613:
-                        // SQL Error Code: 40501
-                        // The service is currently busy. Retry the request after 10 seconds. Code: (reason code to be decoded).
-                        case 40501:
-                        // SQL Error Code: 40197
-                        // The service has encountered an error processing your request. Please try again.
-                        case 40197:
-                        // SQL Error Code: 10929
-                        // Resource ID: %d. The %s minimum guarantee is %d, maximum limit is %d and the current usage for the database is %d.
-                        // However, the server is currently too busy to support requests greater than %d for this database.
-                        // For more information, see http://go.microsoft.com/fwlink/?LinkId=267637. Otherwise, please try again.
-                        case 10929:
-                        // SQL Error Code: 10928
-                        // Resource ID: %d. The %s limit for the database is %d and has been reached. For more information,
-                        // see http://go.microsoft.com/fwlink/?LinkId=267637.
-                        case 10928:
-                        // SQL Error Code: 10060
-                        // A network-related or instance-specific error occurred while establishing a connection to SQL Server.
-                        // The server was not found or was not accessible. Verify that the instance name is correct and that SQL Server
-                        // is configured to allow remote connections. (provider: TCP Provider, error: 0 - A connection attempt failed
-                        // because the connected party did not properly respond after a period of time, or established connection failed
-                        // because connected host has failed to respond.)"}
-                        case 10060:
-                        // SQL Error Code: 10054
-                        // A transport-level error has occurred when sending the request to the server.
-                        // (provider: TCP Provider, error: 0 - An existing connection was forcibly closed by the remote host.)
-                        case 10054:
-                        // SQL Error Code: 10053
-                        // A transport-level error has occurred when receiving results from the server.
-                        // An established connection was aborted by the software in your host machine.
-                        case 10053:
-                        // SQL Error Code: 1205
-                        // Deadlock
-                        case 1205:
-                        // SQL Error Code: 233
-                        // The client was unable to establish a connection because of an error during connection initialization process before login.
-                        // Possible causes include the following: the client tried to connect to an unsupported version of SQL Server;
-                        // the server was too busy to accept new connections; or there was a resource limitation (insufficient memory or maximum
-                        // allowed connections) on the server. (provider: TCP Provider, error: 0 - An existing connection was forcibly closed by
-                        // the remote host.)
-                        case 233:
-                        // SQL Error Code: 121
-                        // The semaphore timeout period has expired
-                        case 121:
-                        // SQL Error Code: 64
-                        // A connection was successfully established with the server, but then an error occurred during the login process.
-                        // (provider: TCP Provider, error: 0 - The specified network name is no longer available.)
-                        case 64:
-                        // DBNETLIB Error Code: 20
-                        // The instance of SQL Server you attempted to connect to does not support encryption.
-                        case 20:
-                        // Login to read - secondary failed due to long wait on 'HADR_DATABASE_WAIT_FOR_TRANSITION_TO_VERSIONING'.
-                        // The replica is not available for login because row versions are missing for transactions that were in-flight
-                        // when the replica was recycled.The issue can be resolved by rolling back or committing the active transactions on
-                        // the primary replica.Occurrences of this condition can be minimized by avoiding long write transactions on the primary.
-                        case 4221:
-                        // Cannot open database "%.*ls" requested by the login. The login failed
-                        case 4060:
-                        // SQL Error Code: 11001
-                        // A network-related or instance-specific error occurred while establishing a connection to SQL Server.
-                        // The server was not found or was not accessible. Verify that the instance name is correct and that SQL
-                        // Server is configured to allow remote connections. (provider: TCP Provider, error: 0 - No such host is known.)
-                        case 11001:
-                            Log.Info("Transient error detected");
-                            return true;
-                            // This exception can be thrown even if the operation completed succesfully, so it's safer to let the application fail.
-                            // DBNETLIB Error Code: -2
-                            // Timeout expired. The timeout period elapsed prior to completion of the operation or the server is not responding. The statement has been terminated.
-                            //case -2:
-                    }
+                    // SQL Error Code: 11001
+                    // A network-related or instance-specific error occurred while establishing a connection to SQL Server.
+                    // The server was not found or was not accessible. Verify that the instance name is correct and that SQL
+                    // Server is configured to allow remote connections. (provider: TCP Provider, error: 0 - No such host is known.)
+                    case 11001:
+                        return true;
                 }
-
-                Log.Info("Transient error not detected");
-
-                return false;
             }
 
-            var timeoutEx = ex is TimeoutException ? true : false;
-
-            if (timeoutEx)
-            {
-                Log.Info("Transient timeout error detected");
-            }
-            else
-            {
-                Log.Info("Transient timeout error not detected");
-            }
-
-            return timeoutEx;
+            return false;
         }
 
         /// <summary>
@@ -3718,7 +3585,7 @@ namespace Quartz.Impl.AdoJobStore
                 }
                 catch (JobPersistenceException jpe)
                 {
-                    if (retry%RetryableActionErrorLogThreshold == 0)
+                    if (retry % RetryableActionErrorLogThreshold == 0)
                     {
                         await schedSignaler.NotifySchedulerListenersError("An error occurred while " + txCallback, jpe, cancellationToken).ConfigureAwait(false);
                     }
