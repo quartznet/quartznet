27249fac49a58adb120fcc1400758c650104ef04
Marko Lahma <marko.lahma@gmail.com>
2020-07-07 14:50:35 +0300

    cleanup

--- src/Quartz.Extensions.DependencyInjection/TriggerExtensions.cs
+++ src/Quartz.Extensions.DependencyInjection/TriggerExtensions.cs
@@ -4,7 +4,9 @@ namespace Quartz
 {
     public static class TriggerExtensions
     {
-        public static IServiceCollectionTriggerConfigurator WithDailyTimeIntervalSchedule(this IServiceCollectionTriggerConfigurator triggerBuilder, Action<DailyTimeIntervalScheduleBuilder>? action = null)
+        public static IServiceCollectionTriggerConfigurator WithDailyTimeIntervalSchedule(
+            this IServiceCollectionTriggerConfigurator triggerBuilder,
+            Action<DailyTimeIntervalScheduleBuilder>? action = null)
         {
             DailyTimeIntervalScheduleBuilder builder = DailyTimeIntervalScheduleBuilder.Create();
             action?.Invoke(builder);
@@ -12,13 +14,17 @@ namespace Quartz
             return triggerBuilder;
         }
 
-        public static IServiceCollectionTriggerConfigurator WithDailyTimeIntervalSchedule(this IServiceCollectionTriggerConfigurator triggerBuilder, DailyTimeIntervalScheduleBuilder schedule)
+        public static IServiceCollectionTriggerConfigurator WithDailyTimeIntervalSchedule(
+            this IServiceCollectionTriggerConfigurator triggerBuilder,
+            DailyTimeIntervalScheduleBuilder schedule)
         {
             triggerBuilder.WithSchedule(schedule);
             return triggerBuilder;
         }
 
-        public static IServiceCollectionTriggerConfigurator WithCalendarIntervalSchedule(this IServiceCollectionTriggerConfigurator triggerBuilder, Action<CalendarIntervalScheduleBuilder>? action = null)
+        public static IServiceCollectionTriggerConfigurator WithCalendarIntervalSchedule(
+            this IServiceCollectionTriggerConfigurator triggerBuilder,
+            Action<CalendarIntervalScheduleBuilder>? action = null)
         {
             CalendarIntervalScheduleBuilder builder = CalendarIntervalScheduleBuilder.Create();
             action?.Invoke(builder);
@@ -26,13 +32,18 @@ namespace Quartz
             return triggerBuilder;
         }
 
-        public static IServiceCollectionTriggerConfigurator WithCalendarIntervalSchedule(this IServiceCollectionTriggerConfigurator triggerBuilder, CalendarIntervalScheduleBuilder schedule)
+        public static IServiceCollectionTriggerConfigurator WithCalendarIntervalSchedule(
+            this IServiceCollectionTriggerConfigurator triggerBuilder,
+            CalendarIntervalScheduleBuilder schedule)
         {
             triggerBuilder.WithSchedule(schedule);
             return triggerBuilder;
         }
 
-        public static IServiceCollectionTriggerConfigurator WithCronSchedule(this IServiceCollectionTriggerConfigurator triggerBuilder, string cronExpression, Action<CronScheduleBuilder>? action = null)
+        public static IServiceCollectionTriggerConfigurator WithCronSchedule(
+            this IServiceCollectionTriggerConfigurator triggerBuilder,
+            string cronExpression,
+            Action<CronScheduleBuilder>? action = null)
         {
             CronScheduleBuilder builder = CronScheduleBuilder.CronSchedule(cronExpression);
             action?.Invoke(builder);
@@ -40,13 +51,17 @@ namespace Quartz
             return triggerBuilder;
         }
 
-        public static IServiceCollectionTriggerConfigurator WithCronSchedule(this IServiceCollectionTriggerConfigurator triggerBuilder, CronScheduleBuilder schedule)
+        public static IServiceCollectionTriggerConfigurator WithCronSchedule(
+            this IServiceCollectionTriggerConfigurator triggerBuilder,
+            CronScheduleBuilder schedule)
         {
             triggerBuilder.WithSchedule(schedule);
             return triggerBuilder;
         }
 
-        public static IServiceCollectionTriggerConfigurator WithSimpleSchedule(this IServiceCollectionTriggerConfigurator triggerBuilder, Action<SimpleScheduleBuilder>? action = null)
+        public static IServiceCollectionTriggerConfigurator WithSimpleSchedule(
+            this IServiceCollectionTriggerConfigurator triggerBuilder,
+            Action<SimpleScheduleBuilder>? action = null)
         {
             SimpleScheduleBuilder builder = SimpleScheduleBuilder.Create();
             action?.Invoke(builder);
@@ -54,7 +69,9 @@ namespace Quartz
             return triggerBuilder;
         }
 
-        public static IServiceCollectionTriggerConfigurator WithSimpleSchedule(this IServiceCollectionTriggerConfigurator triggerBuilder, SimpleScheduleBuilder schedule)
+        public static IServiceCollectionTriggerConfigurator WithSimpleSchedule(
+            this IServiceCollectionTriggerConfigurator triggerBuilder,
+            SimpleScheduleBuilder schedule)
         {
             triggerBuilder.WithSchedule(schedule);
             return triggerBuilder;
