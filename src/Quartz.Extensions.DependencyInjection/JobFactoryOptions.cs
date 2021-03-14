3d4fa39a09d16e30da502b0e047301f33113314c
Artur Dorochowicz <artur@dorochowicz.com>
2021-03-14 22:10:09 +0100

    Obsolete AllowDefaultConstructor instead of removing

--- src/Quartz.Extensions.DependencyInjection/JobFactoryOptions.cs
+++ src/Quartz.Extensions.DependencyInjection/JobFactoryOptions.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace Quartz
 {
     public class JobFactoryOptions
     {
+        /// <summary>
+        /// When DI has not been configured with the job type, should the default no-arg public constructor be tried.
+        /// </summary>
+        [Obsolete("The value is ignored as all jobs are created with ActivatorUtilities.CreateInstance (see https://github.com/quartznet/quartznet/issues/1120).")]
+        public bool AllowDefaultConstructor { get; set; }
+
         /// <summary>
         /// Whether to use scopes when building job instances, enables injection of scoped services like
         /// Entity Framework's DbContext.
