6053359d8756e3dbbd0786272ddf17f9a4bbb28c
Marko Lahma <marko.lahma@gmail.com>
2021-11-14 21:36:24 +0200

    Switch from FAKE to NUKE (#1413)

---
+++ build/Build.CI.GitHubActions.cs
@@ -0,0 +1,30 @@
+using Nuke.Common.CI.GitHubActions;
+
+[GitHubActionsAttribute(
+    "pr",
+    GitHubActionsImage.WindowsLatest,
+    GitHubActionsImage.UbuntuLatest,
+    GitHubActionsImage.MacOsLatest,
+    OnPullRequestBranches = new [] { "main", "v4", "3.x" },
+    OnPullRequestIncludePaths = new[] { "**/*" },
+    OnPullRequestExcludePaths = new[] { "docs/**/*", "package.json", "readme.md" },
+    PublishArtifacts = false,
+    InvokedTargets = new[] { nameof(Compile), nameof(Test), nameof(Pack) },
+    CacheKeyFiles = new[] { "global.json", "src/**/*.csproj", "src/**/package.json" }),
+]
+[GitHubActionsAttribute(
+    "build",
+    GitHubActionsImage.WindowsLatest,
+    GitHubActionsImage.UbuntuLatest,
+    GitHubActionsImage.MacOsLatest,
+    OnPushBranches = new [] { "main" },
+    OnPushIncludePaths = new[] { "**/*" },
+    OnPushExcludePaths = new[] { "docs/**/*", "package.json", "readme.md" },
+    PublishArtifacts = true,
+    InvokedTargets = new[] { nameof(Compile), nameof(Test), nameof(Pack), nameof(Publish) },
+    ImportSecrets = new [] { "NUGET_API_KEY", "MYGET_API_KEY" },
+    CacheKeyFiles = new[] { "global.json", "src/**/*.csproj", "src/**/package.json" })
+]
+public partial class Build
+{
+}
