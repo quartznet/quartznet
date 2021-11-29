a0a526e99ce27f0c8c361ed8d258556e8b230c95
drieseng <gert.driesen@telenet.be>
2021-11-29 23:26:34 +0100

    Use indexer instead of Get(TKey).

--- src/Quartz.Tests.Integration/Impl/AdoJobStore/AdoJobStoreSmokeTest.cs
+++ src/Quartz.Tests.Integration/Impl/AdoJobStore/AdoJobStoreSmokeTest.cs
@@ -548,7 +548,7 @@ namespace Quartz.Tests.Integration.Impl.AdoJobStore
             {
                 try
                 {
-                    ((ManualResetEventSlim) context.Scheduler.Context.Get(KeyResetEvent)).Wait(TimeSpan.FromSeconds(20));
+                    ((ManualResetEventSlim) context.Scheduler.Context[KeyResetEvent]).Wait(TimeSpan.FromSeconds(20));
                     return Task.CompletedTask;
                 }
                 catch (SchedulerException ex)
