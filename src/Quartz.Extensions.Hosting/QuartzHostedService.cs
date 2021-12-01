886cb992d13da3f86617b2e4fb1787376584dae7
Timo van Zijll Langhout <t.zijll@buckaroo.nl>
2021-12-01 17:14:58 +0100

    QuartzHostedService now has jobs start after application startup completes, unless QuartzHostedServiceOptions.AwaitApplicationStarted=false.

--- src/Quartz.Extensions.Hosting/QuartzHostedService.cs
+++ src/Quartz.Extensions.Hosting/QuartzHostedService.cs
@@ -1,6 +1,5 @@
 using System.Threading;
 using System.Threading.Tasks;
-
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
@@ -8,37 +7,88 @@ namespace Quartz
 {
     internal class QuartzHostedService : IHostedService
     {
+        private readonly IApplicationLifetime applicationLifetime;
         private readonly ISchedulerFactory schedulerFactory;
         private readonly IOptions<QuartzHostedServiceOptions> options;
         private IScheduler scheduler = null!;
+        private bool schedulerWasStarted;
 
         public QuartzHostedService(
+            IApplicationLifetime applicationLifetime,
             ISchedulerFactory schedulerFactory,
             IOptions<QuartzHostedServiceOptions> options)
         {
+            this.applicationLifetime = applicationLifetime;
             this.schedulerFactory = schedulerFactory;
             this.options = options;
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
+            // Require successful initialization for application startup to succeed
             scheduler = await schedulerFactory.GetScheduler(cancellationToken);
+
+            if (options.Value.AwaitApplicationStarted) // Sensible mode: proceed with startup, and have jobs start after application startup
+            {
+                // Follow the pattern from BackgroundService.StartAsync: https://github.com/dotnet/runtime/blob/main/src/libraries/Microsoft.Extensions.Hosting.Abstractions/src/BackgroundService.cs
+
+                var deferredSchedulerStartTask = AwaitStartupCompletionAndStartSchedulerAsync(cancellationToken);
+
+                // If the task completed synchronously, await it in order to bubble potential cancellation/failure to the caller
+                // Otherwise, return, allowing application startup to complete
+                if (deferredSchedulerStartTask.IsCompleted)
+                {
+                    await deferredSchedulerStartTask.ConfigureAwait(false);
+                }
+            }
+            else // Legacy mode: start jobs inline
+            {
+                await StartSchedulerAsync(cancellationToken).ConfigureAwait(false);
+            }
+        }
+
+        private async Task AwaitStartupCompletionAndStartSchedulerAsync(CancellationToken startupCancellationToken)
+        {
+            using var combinedCancellationSource = CancellationTokenSource.CreateLinkedTokenSource(startupCancellationToken, applicationLifetime.ApplicationStarted);
+
+            await Task.Delay(Timeout.InfiniteTimeSpan, combinedCancellationSource.Token) // Wait "indefinitely", until startup completes or is aborted
+                .ContinueWith(_ => { }, TaskContinuationOptions.OnlyOnCanceled) // Without an OperationCanceledException on cancellation
+                .ConfigureAwait(false);
+
+            if (!startupCancellationToken.IsCancellationRequested)
+            {
+                await StartSchedulerAsync(applicationLifetime.ApplicationStopping).ConfigureAwait(false); // Startup has finished, but ApplicationStopping may still interrupt starting of the scheduler
+            }
+        }
+
+        /// <summary>
+        /// Starts the <see cref="IScheduler"/>, either immediately or after the delay configured in the <see cref="options"/>.
+        /// </summary>
+        private async Task StartSchedulerAsync(CancellationToken cancellationToken)
+        {
+            schedulerWasStarted = true;
+
+            // Avoid potential race conditions between ourselves and StopAsync, in case it has already made its attempt to stop the scheduler
+            if (applicationLifetime.ApplicationStopping.IsCancellationRequested)
+            {
+                return;
+            }
 
             if (options.Value.StartDelay.HasValue)
             {
-                await scheduler.StartDelayed(options.Value.StartDelay.Value, cancellationToken);
+                await scheduler.StartDelayed(options.Value.StartDelay.Value, cancellationToken).ConfigureAwait(false);
             }
             else
             {
-                await scheduler.Start(cancellationToken);
+                await scheduler.Start(cancellationToken).ConfigureAwait(false);
             }
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
         {
-            if (scheduler != null)
+            if (schedulerWasStarted)
             {
-                await scheduler.Shutdown(options.Value.WaitForJobsToComplete, cancellationToken);
+                await scheduler.Shutdown(options.Value.WaitForJobsToComplete, cancellationToken).ConfigureAwait(false);
             }
         }
     }
--- src/Quartz.Extensions.Hosting/QuartzHostedServiceOptions.cs
+++ src/Quartz.Extensions.Hosting/QuartzHostedServiceOptions.cs
@@ -5,14 +5,25 @@ namespace Quartz
     public class QuartzHostedServiceOptions
     {
         /// <summary>
-        /// if <see langword="true" /> the scheduler will not allow shutdown process
+        /// If <see langword="true" /> the scheduler will not allow shutdown process
         /// to return until all currently executing jobs have completed.
         /// </summary>
         public bool WaitForJobsToComplete { get; set; }
 
         /// <summary>
-        /// if not <see langword="null" /> the scheduler will start after specified delay.
+        /// <para>
+        /// If not <see langword="null" /> the scheduler will start after specified delay.
+        /// </para>
+        /// <para>
+        /// If <see cref="AwaitApplicationStarted"/> is true, the delay starts when application startup completes.
+        /// </para>
         /// </summary>
         public TimeSpan? StartDelay { get; set; }
+
+        /// <summary>
+        /// If true (default), jobs will not be started until application startup completes.
+        /// This avoids the running of jobs <em>during</em> application startup.
+        /// </summary>
+        public bool AwaitApplicationStarted { get; set; } = true;
     }
 }
