172693c80ac14989aeb216473f6cbb2d53fdc7c1
Konstantin Kondr <i@kondr1.ru>
2022-08-19 12:46:12 +0300

    add "UsePersistentStore<T> where T : IJobStore" in DI Extension

--- src/Quartz.Extensions.DependencyInjection/IServiceCollectionQuartzConfigurator.cs
+++ src/Quartz.Extensions.DependencyInjection/IServiceCollectionQuartzConfigurator.cs
@@ -53,6 +53,7 @@ namespace Quartz
 
         void UseInMemoryStore(Action<SchedulerBuilder.InMemoryStoreOptions>? configure = null);
         void UsePersistentStore(Action<SchedulerBuilder.PersistentStoreOptions> configure);
+        void UsePersistentStore<T>(Action<SchedulerBuilder.PersistentStoreOptions> configure) where T : IJobStore;
         void UseThreadPool<T>(Action<SchedulerBuilder.ThreadPoolOptions>? configure = null) where T : IThreadPool;
         void UseDefaultThreadPool(int maxConcurrency, Action<SchedulerBuilder.ThreadPoolOptions>? configure = null);
         void UseDefaultThreadPool(Action<SchedulerBuilder.ThreadPoolOptions>? configure = null);
