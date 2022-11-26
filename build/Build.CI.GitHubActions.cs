f498b3c323a36d22c9a563a97eca9a16c8575691
Marko Lahma <marko.lahma@gmail.com>
2022-11-26 10:58:59 +0200

    Use GNU tar by default in GH Actions

--- build/Build.CI.GitHubActions.cs
+++ build/Build.CI.GitHubActions.cs
@@ -1,6 +1,11 @@
-using Nuke.Common.CI.GitHubActions;
+using System.Collections.Generic;
 
-[GitHubActionsAttribute(
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.CI.GitHubActions.Configuration;
+using Nuke.Common.Execution;
+using Nuke.Common.Utilities;
+
+[CustomGitHubActions(
     "pr",
     GitHubActionsImage.WindowsLatest,
     GitHubActionsImage.UbuntuLatest,
@@ -12,7 +17,7 @@ using Nuke.Common.CI.GitHubActions;
     InvokedTargets = new[] { nameof(Compile), nameof(Test), nameof(Pack) },
     CacheKeyFiles = new[] { "global.json", "src/**/*.csproj", "src/**/package.json" }),
 ]
-[GitHubActionsAttribute(
+[CustomGitHubActions(
     "build",
     GitHubActionsImage.WindowsLatest,
     GitHubActionsImage.UbuntuLatest,
@@ -28,4 +33,41 @@ using Nuke.Common.CI.GitHubActions;
 ]
 public partial class Build
 {
+}
+
+class CustomGitHubActionsAttribute : GitHubActionsAttribute
+{
+    public CustomGitHubActionsAttribute(string name, GitHubActionsImage image, params GitHubActionsImage[] images) : base(name, image, images)
+    {
+    }
+
+    protected override GitHubActionsJob GetJobs(GitHubActionsImage image, IReadOnlyCollection<ExecutableTarget> relevantTargets)
+    {
+        var job = base.GetJobs(image, relevantTargets);
+
+        var newSteps = new List<GitHubActionsStep>(job.Steps);
+        newSteps.Insert(0, new GitHubActionsUseGnuTarStep());
+
+        job.Steps = newSteps.ToArray();
+        return job;
+    }
+}
+
+class GitHubActionsUseGnuTarStep : GitHubActionsStep
+{
+    public override void Write(CustomFileWriter writer)
+    {
+        writer.WriteLine("- if: ${{ runner.os == 'Windows' }}");
+        using (writer.Indent())
+        {
+            writer.WriteLine("name: 'Use GNU tar'");
+            writer.WriteLine("shell: cmd");
+            writer.WriteLine("run: |");
+            using (writer.Indent())
+            {
+                writer.WriteLine("echo \"Adding GNU tar to PATH\"");
+                writer.WriteLine("echo C:\\Program Files\\Git\\usr\\bin>>\"%GITHUB_PATH%\"");
+            }
+        }
+    }
 }
