ab0e0e8b80fbe48eb56c0b4387ec7f491f16625f
stevenaw <sweerdenburg@gmail.com>
2022-12-28 20:42:16 -0500

    Remove unused function

--- src/Quartz/Collections/HashHelpers.cs
+++ src/Quartz/Collections/HashHelpers.cs
@@ -8,14 +8,6 @@ namespace Quartz.Collections
 {
     internal static partial class HashHelpers
     {
-        internal static int PowerOf2(int v)
-        {
-            if ((v & (v - 1)) == 0) return v;
-            int i = 2;
-            while (i < v) i <<= 1;
-            return i;
-        }
-
         // must never be written to
         internal static readonly int[] SizeOneIntArray = new int[1];
 
