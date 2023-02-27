c1d5431a60de6e5c12520545b92c667b76c337d5
Marko Lahma <marko.lahma@gmail.com>
2023-02-27 09:39:55 +0200

    Fix features link and improve how-to code samples

--- docs/.vuepress/configs/navbar/en.ts
+++ docs/.vuepress/configs/navbar/en.ts
@@ -2,7 +2,7 @@ import type { NavbarConfig } from '@vuepress/theme-default'
 
 export const navbarEn: NavbarConfig = [
     { text: 'Home', link: '/' },
-    { text: 'Features', link: 'features' },
+    { text: 'Features', link: '/features.html' },
     /*{ text: 'Blog', link: '/blog' },*/
     { text: 'Discussions', link: 'https://github.com/quartznet/quartznet/discussions' },
     { text: 'NuGet', link: 'https://nuget.org/packages/Quartz' },
