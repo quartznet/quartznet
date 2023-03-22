1ae19fd702762eec735b9f978e6ae9a4afe1bc30
Mikhail Safonov <mishasaf@gmail.com>
2023-03-22 18:48:45 +0300

    ServiceCollectionSchedulerFactory.GetNamedConnectionString passes wrong value to base (#1960)

--- src/Quartz.Extensions.DependencyInjection/ServiceCollectionSchedulerFactory.cs
+++ src/Quartz.Extensions.DependencyInjection/ServiceCollectionSchedulerFactory.cs
@@ -107,7 +107,7 @@ namespace Quartz
                 return connectionString;
             }
 
-            return base.GetNamedConnectionString(connectionString);
+            return base.GetNamedConnectionString(connectionStringName);
         }
 
         protected override T InstantiateType<T>(Type? implementationType)
