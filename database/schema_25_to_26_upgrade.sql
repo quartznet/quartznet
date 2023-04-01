3570fb9dfee1a5aceb6fb6635326f1ea72f1b08b
Jason Finch <jason.finch@gmail.com>
2023-04-02 02:28:35 +1000

    Fix SQL Migration scripts 2.5 to 2.6 missing column (#1985)

--- database/schema_25_to_26_upgrade.sql
+++ database/schema_25_to_26_upgrade.sql
@@ -1,3 +1,7 @@
+/*
+Upgrade Quartz.NET schema for SQL Server database (or other database in commented code)
+Migration from 2.5 to 2.6
+*/
 --
 -- !! FIRST RUN IN TEST ENVIRONMENT AGAINST COPY OF YOU PRODUCTION !!
 --
@@ -9,10 +13,21 @@
 -- you may need to change this syntax depending on your database!
 
 -- sql server
-alter table QRTZ_SIMPROP_TRIGGERS add TIME_ZONE_ID [NVARCHAR] (80);
+
+IF COL_LENGTH('QRTZ_SIMPROP_TRIGGERS','TIME_ZONE_ID') IS NULL
+BEGIN
+  alter table [QRTZ_SIMPROP_TRIGGERS] add TIME_ZONE_ID [NVARCHAR] (80);
+END
+
+IF COL_LENGTH('QRTZ_CRON_TRIGGERS','TIME_ZONE_ID') IS NULL
+BEGIN
+  alter table [QRTZ_CRON_TRIGGERS] add TIME_ZONE_ID [NVARCHAR] (80);
+END
 
 -- mysql
 -- alter table QRTZ_SIMPROP_TRIGGERS add TIME_ZONE_ID VARCHAR(80);
+-- alter table QRTZ_CRON_TRIGGERS add TIME_ZONE_ID VARCHAR(80);
 
 -- oracle
 -- alter table QRTZ_SIMPROP_TRIGGERS add TIME_ZONE_ID VARCHAR2(80);
+-- alter table QRTZ_CRON_TRIGGERS add TIME_ZONE_ID VARCHAR2(80);
