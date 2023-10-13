f33d77f8d1187a707919583df8f52b82535bc6a8
Adam Williams <psc@adamwilliams.eu>
2023-10-13 21:50:29 +0100

    Try and address #1152
    Previously the WithDailyTimeIntervalSchedule name was confusing, and the default interval at 1 minute has led to a number of developers accidentally running tasks every minute of every day (when they really meant every day).

--- src/Quartz/Configuration/TriggerExtensions.cs
+++ src/Quartz/Configuration/TriggerExtensions.cs
@@ -2,11 +2,45 @@ namespace Quartz;
 
 public static class TriggerExtensions
 {
+    /// <summary>
+    /// Sets up a daily schedule with a default interval of *every-minute* unless overridden by the action
+    /// by calling WithInterval / WithIntervalInHours / WithIntervalInMinutes.
+    ///
+    /// For the sake of clarity, you probably want to use WithDailyConfiguredTimeIntervalSchedule instead.
+    /// </summary>
+    [Obsolete("Use WithDailyConfiguredTimeIntervalSchedule with default interval params instead.")]
     public static ITriggerConfigurator WithDailyTimeIntervalSchedule(
         this ITriggerConfigurator triggerBuilder,
         Action<DailyTimeIntervalScheduleBuilder>? action = null)
     {
         DailyTimeIntervalScheduleBuilder builder = DailyTimeIntervalScheduleBuilder.Create();
+        action?.Invoke(builder);
+        triggerBuilder.WithSchedule(builder);
+        return triggerBuilder;
+    }
+    
+    /// <summary>
+    /// Sets up a trigger schedule for one or more occurrences every day.
+    /// With parameter defaults, sets up a daily interval of every 12h.
+    /// </summary>
+    /// <remarks>
+    /// You need to configure the interval for when the trigger fires the job. If you only want one execution per day,
+    /// call EndingDailyAfterCount(1) or set the interval accordingly.
+    /// </remarks>
+    /// <param name="triggerBuilder"></param>
+    /// <param name="action"></param>
+    /// <param name="defaultInterval">The interval count to configure on the builder initially , e.g. 12*hours</param>
+    /// <param name="defaultIntervalUnit">The unit for the defaultInterval count. Defaults to hours.</param>
+    /// <seealso cref="DailyTimeIntervalScheduleBuilder.EndingDailyAfterCount"/>
+    /// <seealso cref="DailyTimeIntervalScheduleBuilder.EndingDailyAt"/>
+    /// <seealso cref="WithCronSchedule(Quartz.ITriggerConfigurator,string,System.Action{Quartz.CronScheduleBuilder}?)"/>
+    /// <returns>Mutated trigger configurator</returns>
+    public static ITriggerConfigurator WithDailyConfiguredTimeIntervalSchedule(
+        this ITriggerConfigurator triggerBuilder,
+        Action<DailyTimeIntervalScheduleBuilder>? action = null, int defaultInterval=12, IntervalUnit defaultIntervalUnit=IntervalUnit.Hour)
+    {
+        DailyTimeIntervalScheduleBuilder builder = DailyTimeIntervalScheduleBuilder.Create();
+        builder.WithInterval(defaultInterval, defaultIntervalUnit);
         action?.Invoke(builder);
         triggerBuilder.WithSchedule(builder);
         return triggerBuilder;
