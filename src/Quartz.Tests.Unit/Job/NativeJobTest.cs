8c0b45082f1658473d26b14d10289f6416a70982
viebrix <33760394+viebrix@users.noreply.github.com>
2024-06-22 08:49:22 +0200

    Add test case for NativeJob

---
+++ src/Quartz.Tests.Unit/Job/NativeJobTest.cs
@@ -0,0 +1,22 @@
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using Quartz.Job;
+
+namespace Quartz.Tests.Unit.Job;
+
+public class NativeJobTest
+{
+    [Test]
+    public void TestNativeJob()
+    {
+        var job = new NativeJob();
+        var context = TestUtil.NewJobExecutionContextFor(job);
+        context.MergedJobDataMap.Put(NativeJob.PropertyCommand, "Test");
+
+        Action act = () => job.Execute(context);
+
+        act.Should().NotThrow<Exception>();
+    }
+}
