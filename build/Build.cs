b627a581bc9e88e15584e32c64d258414706e5bb
Marko Lahma <marko.lahma@gmail.com>
2024-07-03 11:55:40 +0300

    try to restore tests

--- build/Build.cs
+++ build/Build.cs
@@ -181,7 +181,7 @@ partial class Build : NukeBuild, ICompile, IPack
                 .SetConfiguration(configuration)
                 .SetFramework("net8.0")
                 .SetLoggers("GitHubActions")
-                .SetFilter("TestCategory=db-postgres")
+                .SetFilter("\"TestCategory!=db-firebird&TestCategory!=db-oracle&TestCategory!=db-mysql&TestCategory!=db-sqlserver\"")
                 .CombineWith(integrationTestProjects, (_, testProject) => _
                     .SetProjectFile(solution.GetAllProjects(testProject).First())
                 )
