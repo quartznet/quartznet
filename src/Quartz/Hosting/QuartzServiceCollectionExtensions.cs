f6df8b3feb8505de837d7ffd2de75d69ec28a237
Marko Lahma <marko.lahma@gmail.com>
2024-07-31 12:08:13 +0300

    tweaks

--- src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
+++ src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
@@ -1,12 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
 
-#if NET8_0_OR_GREATER
-using System.Diagnostics.CodeAnalysis;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-#else
-using Microsoft.Extensions.Hosting;
-#endif
-
 namespace Quartz;
 
 public static class QuartzServiceCollectionExtensions
@@ -25,28 +18,6 @@ public static class QuartzServiceCollectionExtensions
             services.Configure(configure);
         }
 
-#if NET8_0_OR_GREATER
         return services.AddHostedService<QuartzHostedService>();
-#else
-        return services.AddSingleton<IHostedService, QuartzHostedService>();
-#endif
     }
-
-#if NET8_0_OR_GREATER
-    /// <summary>
-    /// Add an <see cref="QuartzHostedService"/> and implementation <see cref="IQuartzHostedLifecycleService"/>> to the <see cref="IServiceCollection"/>.
-    /// </summary>
-    /// <typeparam name="THostedLifecycleService">Type implementing the <see cref="IQuartzHostedLifecycleService"/> interface.</typeparam>
-    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
-    /// <param name="configure">A delegate that is used to configure an <see cref="QuartzHostedServiceOptions"/>.</param>
-    public static IServiceCollection AddQuartzHostedService<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] THostedLifecycleService>(
-        this IServiceCollection services,
-        Action<QuartzHostedServiceOptions>? configure = null)
-        where THostedLifecycleService : class, IQuartzHostedLifecycleService
-    {
-        services.TryAddSingleton<IQuartzHostedLifecycleService, THostedLifecycleService>();
-
-        return services.AddQuartzHostedService(configure);
-    }
-#endif
 }
