99456b4bef9da69d720218ae87a10b50911c3c08
Marko Lahma <marko.lahma@gmail.com>
2024-07-31 11:04:24 +0300

    Make scheduler registration cleanup more robust during shutdown

--- src/Quartz.Extensions.Hosting/QuartzHostedService.cs
+++ src/Quartz.Extensions.Hosting/QuartzHostedService.cs
@@ -10,114 +10,108 @@ using Lifetime = Microsoft.Extensions.Hosting.IHostApplicationLifetime;
 using Lifetime = Microsoft.Extensions.Hosting.IApplicationLifetime;
 #endif
 
-namespace Quartz
+namespace Quartz;
+
+public sealed class QuartzHostedService : IHostedService
 {
-    public sealed class QuartzHostedService : IHostedService
+    private readonly Lifetime applicationLifetime;
+    private readonly ISchedulerFactory schedulerFactory;
+    private readonly IOptions<QuartzHostedServiceOptions> options;
+    private IScheduler? scheduler;
+    internal Task? startupTask;
+
+    public QuartzHostedService(
+        Lifetime applicationLifetime,
+        ISchedulerFactory schedulerFactory,
+        IOptions<QuartzHostedServiceOptions> options)
     {
-        private readonly Lifetime applicationLifetime;
-        private readonly ISchedulerFactory schedulerFactory;
-        private readonly IOptions<QuartzHostedServiceOptions> options;
-        private IScheduler? scheduler;
-        internal Task? startupTask;
-        private bool schedulerWasStarted;
+        this.applicationLifetime = applicationLifetime;
+        this.schedulerFactory = schedulerFactory;
+        this.options = options;
+    }
 
-        public QuartzHostedService(
-            Lifetime applicationLifetime,
-            ISchedulerFactory schedulerFactory,
-            IOptions<QuartzHostedServiceOptions> options)
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        // Require successful initialization for application startup to succeed
+        scheduler = await schedulerFactory.GetScheduler(cancellationToken);
+
+        if (options.Value.AwaitApplicationStarted) // Sensible mode: proceed with startup, and have jobs start after application startup
         {
-            this.applicationLifetime = applicationLifetime;
-            this.schedulerFactory = schedulerFactory;
-            this.options = options;
-        }
+            // Follow the pattern from BackgroundService.StartAsync: https://github.com/dotnet/runtime/blob/main/src/libraries/Microsoft.Extensions.Hosting.Abstractions/src/BackgroundService.cs
 
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            // Require successful initialization for application startup to succeed
-            scheduler = await schedulerFactory.GetScheduler(cancellationToken);
+            startupTask = AwaitStartupCompletionAndStartSchedulerAsync(cancellationToken);
 
-            if (options.Value.AwaitApplicationStarted) // Sensible mode: proceed with startup, and have jobs start after application startup
+            // If the task completed synchronously, await it in order to bubble potential cancellation/failure to the caller
+            // Otherwise, return, allowing application startup to complete
+            if (startupTask.IsCompleted)
             {
-                // Follow the pattern from BackgroundService.StartAsync: https://github.com/dotnet/runtime/blob/main/src/libraries/Microsoft.Extensions.Hosting.Abstractions/src/BackgroundService.cs
-
-                startupTask = AwaitStartupCompletionAndStartSchedulerAsync(cancellationToken);
-
-                // If the task completed synchronously, await it in order to bubble potential cancellation/failure to the caller
-                // Otherwise, return, allowing application startup to complete
-                if (startupTask.IsCompleted)
-                {
-                    await startupTask.ConfigureAwait(false);
-                }
-            }
-            else // Legacy mode: start jobs inline
-            {
-                startupTask = StartSchedulerAsync(cancellationToken);
                 await startupTask.ConfigureAwait(false);
             }
         }
-
-        private async Task AwaitStartupCompletionAndStartSchedulerAsync(CancellationToken startupCancellationToken)
+        else // Legacy mode: start jobs inline
         {
-            using var combinedCancellationSource = CancellationTokenSource.CreateLinkedTokenSource(startupCancellationToken, applicationLifetime.ApplicationStarted);
+            startupTask = StartSchedulerAsync(cancellationToken);
+            await startupTask.ConfigureAwait(false);
+        }
+    }
 
-            await Task.Delay(Timeout.InfiniteTimeSpan, combinedCancellationSource.Token) // Wait "indefinitely", until startup completes or is aborted
-                .ContinueWith(_ => { }, TaskContinuationOptions.OnlyOnCanceled) // Without an OperationCanceledException on cancellation
-                .ConfigureAwait(false);
+    private async Task AwaitStartupCompletionAndStartSchedulerAsync(CancellationToken startupCancellationToken)
+    {
+        using var combinedCancellationSource = CancellationTokenSource.CreateLinkedTokenSource(startupCancellationToken, applicationLifetime.ApplicationStarted);
 
-            if (!startupCancellationToken.IsCancellationRequested)
-            {
-                await StartSchedulerAsync(applicationLifetime.ApplicationStopping).ConfigureAwait(false); // Startup has finished, but ApplicationStopping may still interrupt starting of the scheduler
-            }
+        await Task.Delay(Timeout.InfiniteTimeSpan, combinedCancellationSource.Token) // Wait "indefinitely", until startup completes or is aborted
+            .ContinueWith(_ => { }, TaskContinuationOptions.OnlyOnCanceled) // Without an OperationCanceledException on cancellation
+            .ConfigureAwait(false);
+
+        if (!startupCancellationToken.IsCancellationRequested)
+        {
+            await StartSchedulerAsync(applicationLifetime.ApplicationStopping).ConfigureAwait(false); // Startup has finished, but ApplicationStopping may still interrupt starting of the scheduler
+        }
+    }
+
+    /// <summary>
+    /// Starts the <see cref="IScheduler"/>, either immediately or after the delay configured in the <see cref="options"/>.
+    /// </summary>
+    private async Task StartSchedulerAsync(CancellationToken cancellationToken)
+    {
+        if (scheduler is null)
+        {
+            throw new InvalidOperationException("The scheduler should have been initialized first.");
         }
 
-        /// <summary>
-        /// Starts the <see cref="IScheduler"/>, either immediately or after the delay configured in the <see cref="options"/>.
-        /// </summary>
-        private async Task StartSchedulerAsync(CancellationToken cancellationToken)
+        // Avoid potential race conditions between ourselves and StopAsync, in case it has already made its attempt to stop the scheduler
+        if (applicationLifetime.ApplicationStopping.IsCancellationRequested)
         {
-            if (scheduler is null)
-            {
-                throw new InvalidOperationException("The scheduler should have been initialized first.");
-            }
-
-            schedulerWasStarted = true;
-
-            // Avoid potential race conditions between ourselves and StopAsync, in case it has already made its attempt to stop the scheduler
-            if (applicationLifetime.ApplicationStopping.IsCancellationRequested)
-            {
-                return;
-            }
-
-            if (options.Value.StartDelay.HasValue)
-            {
-                await scheduler.StartDelayed(options.Value.StartDelay.Value, cancellationToken).ConfigureAwait(false);
-            }
-            else
-            {
-                await scheduler.Start(cancellationToken).ConfigureAwait(false);
-            }
+            return;
         }
 
-        public async Task StopAsync(CancellationToken cancellationToken)
+        if (options.Value.StartDelay.HasValue)
         {
-            // Stopped without having been started
-            if (scheduler is null || startupTask is null)
-            {
-                return;
-            }
+            await scheduler.StartDelayed(options.Value.StartDelay.Value, cancellationToken).ConfigureAwait(false);
+        }
+        else
+        {
+            await scheduler.Start(cancellationToken).ConfigureAwait(false);
+        }
+    }
 
-            try
-            {
-                // Wait until any ongoing startup logic has finished or the graceful shutdown period is over
-                await Task.WhenAny(startupTask, Task.Delay(Timeout.Infinite, cancellationToken)).ConfigureAwait(false);
-            }
-            finally
-            {
-                if (schedulerWasStarted && !cancellationToken.IsCancellationRequested)
-                {
-                    await scheduler.Shutdown(options.Value.WaitForJobsToComplete, cancellationToken).ConfigureAwait(false);
-                }
-            }
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        // Stopped without having been started
+        if (scheduler is null || startupTask is null)
+        {
+            return;
+        }
+
+        try
+        {
+            // Wait until any ongoing startup logic has finished or the graceful shutdown period is over
+            await Task.WhenAny(startupTask, Task.Delay(Timeout.Infinite, cancellationToken)).ConfigureAwait(false);
+        }
+        finally
+        {
+            // we always need to call shutdown to ensure that we unbind the scheduler from global repository
+            await scheduler.Shutdown(options.Value.WaitForJobsToComplete, cancellationToken).ConfigureAwait(false);
         }
     }
 }
