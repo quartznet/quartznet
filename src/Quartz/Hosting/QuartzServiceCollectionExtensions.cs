2208784d5c949a36d4458739c009831f9fd31c51
ANDANTINO <AHT_666@mail.ru>
2024-08-14 18:34:02 +0500

    Added a new extension method in QuartzServiceCollectionExtensions class

--- src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
+++ src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Quartz;
@@ -19,5 +20,24 @@ public static class QuartzServiceCollectionExtensions
         }
 
         return services.AddHostedService<QuartzHostedService>();
+    }
+
+    /// <summary>
+    /// Add an <see cref="QuartzHostedService"/> to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <typeparam name="TExtendedHostedService">Type extending the <see cref="QuartzHostedService"/> class.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+    /// <param name="configure">A delegate that is used to configure an <see cref="QuartzHostedServiceOptions"/>.</param>
+    public static IServiceCollection AddQuartzHostedService<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TExtendedHostedService>(
+        this IServiceCollection services,
+        Action<QuartzHostedServiceOptions>? configure = null)
+        where TExtendedHostedService : QuartzHostedService
+    {
+        if (configure is not null)
+        {
+            services.Configure(configure);
+        }
+
+        return services.AddHostedService<TExtendedHostedService>();
     }
 }
