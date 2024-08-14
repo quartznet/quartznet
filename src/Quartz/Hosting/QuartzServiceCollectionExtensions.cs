34ae91a9df3d6df67306a6d15fd891974b0f2ba7
Marko Lahma <marko.lahma@gmail.com>
2024-08-14 21:33:19 +0300

    tweak

--- src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
+++ src/Quartz/Hosting/QuartzServiceCollectionExtensions.cs
@@ -14,30 +14,25 @@ public static class QuartzServiceCollectionExtensions
         this IServiceCollection services,
         Action<QuartzHostedServiceOptions>? configure = null)
     {
-        if (configure is not null)
-        {
-            services.Configure(configure);
-        }
-
-        return services.AddHostedService<QuartzHostedService>();
+        return services.AddQuartzHostedService<QuartzHostedService>(configure);
     }
 
     /// <summary>
     /// Add an <see cref="QuartzHostedService"/> to the <see cref="IServiceCollection"/>.
     /// </summary>
-    /// <typeparam name="TExtendedHostedService">Type extending the <see cref="QuartzHostedService"/> class.</typeparam>
+    /// <typeparam name="T">Type extending the <see cref="QuartzHostedService"/> class.</typeparam>
     /// <param name="services">The <see cref="IServiceCollection"/>.</param>
     /// <param name="configure">A delegate that is used to configure an <see cref="QuartzHostedServiceOptions"/>.</param>
-    public static IServiceCollection AddQuartzHostedService<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TExtendedHostedService>(
+    public static IServiceCollection AddQuartzHostedService<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>(
         this IServiceCollection services,
         Action<QuartzHostedServiceOptions>? configure = null)
-        where TExtendedHostedService : QuartzHostedService
+        where T : QuartzHostedService
     {
         if (configure is not null)
         {
             services.Configure(configure);
         }
 
-        return services.AddHostedService<TExtendedHostedService>();
+        return services.AddHostedService<T>();
     }
 }
