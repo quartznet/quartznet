9f8760d99c50ae04c4282b89a23b56634de3c7a5
Marko Lahma <marko.lahma@gmail.com>
2024-09-24 21:31:14 +0300

    Use invariant culture for StdAdoConstants SQL formatting

---
+++ src/Quartz.Tests.Unit/Impl/AdoJobStore/StdAdoConstantsTest.cs
@@ -0,0 +1,23 @@
+using System.Globalization;
+using System.Threading;
+
+using FluentAssertions;
+
+using NUnit.Framework;
+
+using Quartz.Impl.AdoJobStore;
+
+namespace Quartz.Tests.Unit.Impl.AdoJobStore;
+
+public class StdAdoConstantsTest
+{
+    [Test]
+    public void ShouldProduceResultsInInvariantCulture()
+    {
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("lt-LT");
+
+        var sql = StdAdoConstants.SqlSelectMisfiredTriggers;
+
+        sql.Should().Be("SELECT * FROM {0}TRIGGERS WHERE SCHED_NAME = @schedulerName AND MISFIRE_INSTR <> -1 AND NEXT_FIRE_TIME < @nextFireTime ORDER BY NEXT_FIRE_TIME ASC, PRIORITY DESC");
+    }
+}
