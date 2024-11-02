83ee5ac30de5c0548fe1fea7e5069bba84b37d5f
Marko Lahma <marko.lahma@gmail.com>
2024-11-02 08:32:00 +0200

    Upgrade NuGet packages

--- src/Quartz.Server/Program.cs
+++ src/Quartz.Server/Program.cs
@@ -21,7 +21,7 @@ namespace Quartz.Server
             // change from service account's dir to more logical one
             Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
 
-            var logRepository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
+            var logRepository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly()!);
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
             HostFactory.Run(x =>
---
+++ src/Quartz.Tests.Unit/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Assert = NUnit.Framework.Legacy.ClassicAssert;
+global using CollectionAssert = NUnit.Framework.Legacy.CollectionAssert;
+global using StringAssert = NUnit.Framework.Legacy.StringAssert;
+global using DirectoryAssert = NUnit.Framework.Legacy.DirectoryAssert;
+global using FileAssert = NUnit.Framework.Legacy.FileAssert;
